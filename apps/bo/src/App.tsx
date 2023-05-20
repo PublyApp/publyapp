bedad8d9d391658d512302dc2b91b5a65580b3f9
Andrianarisoa Daniel <radandevist@gmail.com>
2023-05-20 10:20:56 +0300

    airbnb eslint config guideline

--- apps/bo/src/App.tsx
+++ apps/bo/src/App.tsx
@@ -1,21 +1,13 @@
-import React from 'react';
-
 import { Button } from '@devist/ui-react';
 
 import image from './assets/react.svg';
 
 function App() {
-	const [count, setCount] = React.useState(0);
-
 	return (
 		<>
 			<img src={image} alt="cool" />
-			<h1>This is the bo, count: {count}</h1>
-			<Button
-				onClick={() => {
-					setCount(count + 1);
-				}}
-			/>
+			<h1>This is the bo</h1>
+			<Button />
 		</>
 	);
 }
