beac2c90fcff405fce410dd264bb0c28a988963f
Andrianarisoa Daniel <radandevist@gmail.com>
2023-06-25 09:09:34 +0300

    wip posts

--- apps/front/src/pages/index.tsx
+++ apps/front/src/pages/index.tsx
@@ -1,10 +1,13 @@
 import { Typography } from '@mui/material';
 
+import PostsList from '../components/post/PostsList';
+
 const Home = () => {
 	return (
 		<>
-			<Typography variant="h1">Hello Dude</Typography>
-			<Typography variant="h3">Welcome home</Typography>
+			{/* <Typography variant="h1">Hello Dude</Typography>
+			<Typography variant="h3">Welcome home</Typography> */}
+			<PostsList />
 		</>
 	);
 };
