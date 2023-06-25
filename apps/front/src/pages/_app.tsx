8fe50adf64e52ae50be6ec5f871e148b4e453295
Andrianarisoa Daniel <radandevist@gmail.com>
2023-06-25 12:17:21 +0300

    wip: front react query

--- apps/front/src/pages/_app.tsx
+++ apps/front/src/pages/_app.tsx
@@ -1,8 +1,11 @@
+import { useState } from 'react';
+
 import CssBaseline from '@mui/material/CssBaseline';
 // import { ThemeProvider } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { HydrationBoundary, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { theme } from '@aktiveo/ui-react/utils/theme';
 import '@aktiveo/ui-react/styles/fonts.css';
@@ -10,17 +13,27 @@ import '@aktiveo/ui-react/styles/fonts.css';
 import Layout from '../components/layout/Layout';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+	const [queryClient] = useState(() => {
+		return new QueryClient();
+	});
+
 	return (
-		<ThemeProvider theme={theme}>
+		<>
 			<Head>
 				<title>Aktiveo</title>
 			</Head>
-			<CssBaseline />
-			<Layout>
-				{/* eslint-disable-next-line react/jsx-props-no-spreading */}
-				<Component {...pageProps} />
-			</Layout>
-		</ThemeProvider>
+			<QueryClientProvider client={queryClient}>
+				<HydrationBoundary state={pageProps.dehydratedState}>
+					<ThemeProvider theme={theme}>
+						<CssBaseline />
+						<Layout>
+							{/* eslint-disable-next-line react/jsx-props-no-spreading */}
+							<Component {...pageProps} />
+						</Layout>
+					</ThemeProvider>
+				</HydrationBoundary>
+			</QueryClientProvider>
+		</>
 	);
 };
 
