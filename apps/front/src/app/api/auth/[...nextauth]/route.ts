1c2fcae6f2cbaaebe4c6fa1897f57a9c40de77bd
Andrianarisoa Daniel <radandevist@gmail.com>
2023-07-29 18:44:40 +0300

    failed to use next-auth

--- apps/front/src/app/api/auth/[...nextauth]/route.ts
+++ apps/front/src/app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,8 @@
 import NextAuth from 'next-auth';
-import EmailProvider from 'next-auth/providers/email';
 
-import { NextAuthAdapterParse } from '../../../../utils/NextAuthAdapterParse';
+import { authOptions } from './options';
 // import GithubProvider from 'next-auth/providers/github';
 
-// export const authOptions = {
-// 	// Configure one or more authentication providers
-// 	providers: [
-// 		// GithubProvider({
-// 		// 	clientId: process.env.GITHUB_ID,
-// 		// 	clientSecret: process.env.GITHUB_SECRET,
-// 		// }),
-// 		// ...add more providers here
-// 	],
-// };
-export default NextAuth({
-	providers: [
-		EmailProvider({
-			server: process.env.EMAIL_SERVER,
-			from: process.env.EMAIL_FROM,
-		}),
-	],
-	adapter: NextAuthAdapterParse('http://localhost:6180/parse', 'aktiveo', undefined, 'local-master-key'),
-});
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
