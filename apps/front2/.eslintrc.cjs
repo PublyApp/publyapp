d36406e0c2063d0b8f025650aed5a5db125a5fe5
Andrianarisoa Daniel <radandevist@gmail.com>
2023-08-23 09:57:02 +0300

    init vite ssr

---
+++ apps/front2/.eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+	root: true,
+	// rules: {
+	//   'react-refresh/only-export-components': 'warn'
+	// },
+	reportUnusedDisableDirectives: true,
+	// ignorePatterns: ['dist/*'],
+	env: { browser: true, es2020: true, node: true },
+	// parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	// settings: { react: { version: 'detect' } },
+	// plugins: ['react-refresh'],
+	extends: [
+		// 'eslint:recommended',
+		// 'plugin:react/recommended',
+		// 'plugin:react/jsx-runtime',
+		// 'plugin:react-hooks/recommended',
+		// 'plugin:@typescript-eslint/recommended'
+		'custom-react-vite',
+	],
+	// parser: '@typescript-eslint/parser'
+};
