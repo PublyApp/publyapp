5a89543d854ba2dc52886b3c89e3267f6ba7ed34
Andrianarisoa Daniel <radandevist@gmail.com>
2023-08-24 08:01:52 +0300

    feat(front2): add vite-plugin-ssr frontend experiment

---
+++ apps/front2/server/root.ts
@@ -0,0 +1,10 @@
+// https://stackoverflow.com/questions/46745014/alternative-for-dirname-in-node-when-using-the-experimental-modules-flag/50052194#50052194
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const root = `${__dirname}/..`;
+
+export { root };
