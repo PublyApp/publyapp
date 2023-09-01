6b68155dd7f34c7e3e442fd59d3734d097695af7
Andrianarisoa Daniel <radandevist@gmail.com>
2023-09-01 19:40:48 +0300

    update @mui

--- apps/bo/src/containers/designSystem/ButtonsPage.tsx
+++ apps/bo/src/containers/designSystem/ButtonsPage.tsx
@@ -3,13 +3,11 @@ import { Button } from '@mui/material';
 const ButtonsPage = () => {
 	return (
 		<>
-			<Button variant="contained" raised={1}>
-				lol
-			</Button>
+			<Button variant="contained" /* raised={1} */>lol</Button>
 			<Button variant="contained" color="info">
 				lol
 			</Button>
-			<Button variant="contained" color="warning" raised={1}>
+			<Button variant="contained" color="warning" /*  raised={1} */>
 				lol
 			</Button>
 			<Button variant="outlined">lol</Button>
