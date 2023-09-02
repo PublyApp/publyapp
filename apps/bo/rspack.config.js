152c592500149e5b91d9bffd49d87b1c9245a29a
Andrianarisoa Daniel <radandevist@gmail.com>
2023-09-02 10:39:00 +0300

    dotenv with rspack

--- apps/bo/rspack.config.js
+++ apps/bo/rspack.config.js
@@ -1,9 +1,24 @@
-const { EnvironmentPlugin } = require('@rspack/core');
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
 const dotenv = require('dotenv');
 const dotenvExpand = require('dotenv-expand');
+const { EnvironmentPlugin } = require('@rspack/core');
 
-const envConfig = dotenv.config({ path: path.resolve(__dirname, '..', envFileName) });
-dotenvExpand.expand(envConfig);
+const isLocal = process.env.APP_ENV === 'local';
+const isProduction = process.env.APP_ENV === 'production';
+
+let envFileName = '.env.local';
+
+if (isProduction) {
+	envFileName = '.env.production';
+} else if (!isProduction && !isLocal) {
+	envFileName = '.env.preprod';
+}
+
+const envConfig = dotenv.config({ path: path.resolve(__dirname, envFileName) });
+const { parsed: env } = dotenvExpand.expand(envConfig);
 
 /** @type {import('@rspack/cli').Configuration} */
 const config = {
@@ -24,6 +39,7 @@ const config = {
 			],
 		},
 	},
+	plugins: [new EnvironmentPlugin(env)],
 	devServer: {
 		port: 6182,
 	},
