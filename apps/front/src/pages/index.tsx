e3f963f55f9b0f28c2c8eec23878dba75efbf60a
Andrianarisoa Daniel <radandevist@gmail.com>
2023-09-15 21:56:28 +0300

    integrate template header in front

--- apps/front/src/pages/index.tsx
+++ apps/front/src/pages/index.tsx
@@ -1,12 +1,15 @@
 import Typography from '@mui/material/Typography';
 
-import DefaultLayoutFront from '../components/layout/DefaultLayoutFront';
+import MainLayout from '../components/layout/MainLayout';
+
+// import ProductsView from '../components/ProductsView';
 
 const Home = () => {
 	return (
-		<DefaultLayoutFront>
-			<Typography>Welcome home!!</Typography>
-		</DefaultLayoutFront>
+		<MainLayout>
+			{/* <ProductsView /> */}
+			<Typography variant="h1">Next to do is productzs list view with filer on the side</Typography>
+		</MainLayout>
 	);
 };
 
