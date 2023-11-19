a3f6615a03d2f51f148db261386310ce616e5c38
Andrianarisoa Daniel <radandevist@gmail.com>
2023-11-19 13:34:36 +0300

    last checks before deploy

--- apps/awsm/src/env.ts
+++ apps/awsm/src/env.ts
@@ -1,22 +1,16 @@
 import z from 'zod';
 
-import { numericStringSchema } from '@devist/shared/lib/zod';
+import { numericStringSchema } from '@shared/lib/zod';
 
 export const envSchema = z.object({
 	PORT: numericStringSchema,
 	SERVER_URL: z.string(),
-	DATABASE_URI: z.string(),
-	REST_API_KEY: z.string(),
-	EXPRESS_FILES_MOUNT_PATH: z.string(),
 	// ===
 	PARSE_APP_ID: z.string(),
 	PARSE_PATH: z.string(),
 	PARSE_SERVER_URL: z.string(),
 	PARSE_MASTER_KEY: z.string(),
 	// ===
-	CLOUDINARY_NAME: z.string(),
-	CLOUDINARY_API_KEY: z.string(),
-	CLOUDINARY_API_SECRET: z.string(),
 });
 
 export type AppEnv = z.infer<typeof envSchema>;
