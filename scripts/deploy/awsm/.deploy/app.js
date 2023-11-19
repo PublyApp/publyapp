95f2b9f113f44b6dee731374dc6cbc607ba52ecd
Andrianarisoa Daniel <radandevist@gmail.com>
2023-11-19 12:31:16 +0300

    new workflow for awesome app & update deploy builds

--- scripts/deploy/awsm/.deploy/app.js
+++ scripts/deploy/awsm/.deploy/app.js
@@ -1,4 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 // eslint-disable-next-line import/no-unresolved, import/extensions
-require('./apps/server/dist/index');
+require('./apps/awsm/dist/index');
