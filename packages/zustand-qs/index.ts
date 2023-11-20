eb272d2d335673138ba3f24041857949a7042d23
Andrianarisoa Daniel <radandevist@gmail.com>
2023-11-20 09:51:33 +0300

    prepare deploy

--- packages/zustand-qs/index.ts
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/consistent-type-exports */
-export { querystring, QueryStringOptions } from './middleware.js';
-export { stringify, parse } from './parser.js';
-export { createURL } from './utils.js';
