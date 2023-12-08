80118a905a1049b9b3675ff9ca25a1936c4aeb27
Andrianarisoa Daniel <radandevist@gmail.com>
2023-12-08 05:16:36 +0300

    chore(front): remove next.js app ahead of remix migration

--- apps/front/src/app/products/page.tsx
+++
@@ -1,7 +0,0 @@
-import ProductsView from '@front/components/commerce/ProductsView';
-
-const HomePage = () => {
-	return <ProductsView />;
-};
-
-export default HomePage;
--- apps/front/src/lib/i18n.ts
+++
@@ -1,5 +0,0 @@
-'use client';
-
-import { initReactLocalization } from '@devist/ui-react/lib/i18n';
-
-initReactLocalization();
