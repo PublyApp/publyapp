28a343587495bff8030d445044fdf8097219f377
Andrianarisoa Daniel <radandevist@gmail.com>
2023-12-08 05:37:22 +0300

    bring template

---
+++ apps/front/app/src/Layout.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+import Copyright from './Copyright';
+import ProTip from './ProTip';
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<Container maxWidth="sm">
+			<Box sx={{ my: 4 }}>
+				{children}
+				<ProTip />
+				<Copyright />
+			</Box>
+		</Container>
+	);
+}
