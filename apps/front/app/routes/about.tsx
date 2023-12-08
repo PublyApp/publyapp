a8afb301541f0c4b28bedcb52a3ca2f152e0e376
Andrianarisoa Daniel <radandevist@gmail.com>
2023-12-08 05:37:22 +0300

    bring template

---
+++ apps/front/app/routes/about.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Link } from '@remix-run/react';
+
+export default function About() {
+	return (
+		<React.Fragment>
+			<Typography variant="h4" component="h1" gutterBottom>
+				Material UI Remix in TypeScript example
+			</Typography>
+			<Button variant="contained" component={Link} to="/">
+				Go to the main page
+			</Button>
+		</React.Fragment>
+	);
+}
