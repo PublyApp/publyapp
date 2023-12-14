afdbbb966bb9139eafa7dbf4d3753dcb4d9c488a
Andrianarisoa Daniel <radandevist@gmail.com>
2023-12-14 20:35:15 +0300

    redirect index

--- apps/front/app/routes/_index.tsx
+++ apps/front/app/routes/_index.tsx
@@ -1,29 +1,21 @@
-// import { Link, Typography } from '@mui/material';
-import type { MetaFunction } from '@remix-run/node';
+import { redirect, type ActionFunction } from '@remix-run/node';
 
-import AwesomeToolsView from '@/front/containers/awesomeTools/AwesomeToolsView';
+// import type { MetaFunction } from '@remix-run/node';
 
-// import { Link as RemixLink } from '@remix-run/react';
+// import AwesomeToolsView from '@/front/containers/awesomeTools/AwesomeToolsView';
 
-// https://remix.run/docs/en/main/route/meta
-export const meta: MetaFunction = () => {
-	return [{ title: 'Remix Starter' }, { name: 'description', content: 'Welcome to remix!' }];
+// // https://remix.run/docs/en/main/route/meta
+// export const meta: MetaFunction = () => {
+// 	return [{ title: 'Remix Starter' }, { name: 'description', content: 'Welcome to remix!' }];
+// };
+
+export const loader: ActionFunction = async () => {
+	return redirect('/awesome-tools', 301);
 };
 
 // https://remix.run/docs/en/main/file-conventions/routes#basic-routes
 const Index = () => {
-	return (
-		<>
-			{/* <Typography variant="h4" component="h1" gutterBottom>
-				Material UI Remix in TypeScript example
-			</Typography>
-			<Link to="/about" color="secondary" component={RemixLink}>
-				Go to the about page
-			</Link> */}
-			{/* <p>lol</p> */}
-			<AwesomeToolsView />
-		</>
-	);
+	return null;
 };
 
 export default Index;
