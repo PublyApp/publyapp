8aa18d8b1bb9e0c4f0fecc0783971ba5ee686711
Andrianarisoa Daniel <radandevist@gmail.com>
2023-12-14 22:22:28 +0300

    add features on header

---
+++ apps/front/app/layouts/common/Searchbar.tsx
@@ -0,0 +1,84 @@
+import { useState } from 'react';
+
+import { Button, ClickAwayListener, IconButton, Input, InputAdornment, Slide, type SxProps } from '@mui/material';
+import { alpha, styled, type Theme } from '@mui/material/styles';
+
+import Iconify from '@devist/ui-react/components/Iconify';
+
+import { HEADER } from '@/front/lib/constants';
+
+// ----------------------------------------------------------------------
+
+const StyledSearchbar = styled('div')(({ theme }) => {
+	return {
+		top: 0,
+		left: 0,
+		zIndex: 99,
+		width: '100%',
+		display: 'flex',
+		position: 'absolute',
+		alignItems: 'center',
+		height: HEADER.H_MOBILE,
+		backdropFilter: 'blur(6px)',
+		WebkitBackdropFilter: 'blur(6px)', // Fix on Mobile
+		padding: theme.spacing(0, 3),
+		boxShadow: theme.customShadows.z8,
+		backgroundColor: `${alpha(theme.palette.background.default, 0.72)}`,
+		[theme.breakpoints.up('md')]: {
+			height: HEADER.H_MAIN_DESKTOP,
+			padding: theme.spacing(0, 5),
+		},
+	};
+});
+
+// ----------------------------------------------------------------------
+
+type SearchbarProps = {
+	sx?: SxProps<Theme>;
+};
+
+const Searchbar = ({ sx }: SearchbarProps) => {
+	const [open, setOpen] = useState(false);
+
+	const handleOpen = () => {
+		setOpen((prev) => {
+			return !prev;
+		});
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	return (
+		<ClickAwayListener onClickAway={handleClose}>
+			<div>
+				<IconButton color="inherit" aria-label="search" onClick={handleOpen} sx={sx}>
+					<Iconify icon="carbon:search" />
+				</IconButton>
+
+				<Slide direction="down" in={open} mountOnEnter unmountOnExit>
+					<StyledSearchbar>
+						<Input
+							autoFocus
+							fullWidth
+							disableUnderline
+							placeholder="Search…"
+							startAdornment={
+								<InputAdornment position="start">
+									<Iconify icon="carbon:search" sx={{ color: 'text.disabled' }} />
+								</InputAdornment>
+							}
+							sx={{ mr: 1, fontWeight: 'fontWeightBold' }}
+						/>
+						<Button variant="contained" onClick={handleClose}>
+							Search
+						</Button>
+					</StyledSearchbar>
+				</Slide>
+			</div>
+		</ClickAwayListener>
+	);
+};
+
+export default Searchbar;
