6b9c8b40c13bd5eb938153cde75ed3c226139893
Andrianarisoa Daniel <radandevist@gmail.com>
2023-12-23 18:12:31 +0300

    feat(office): refine file manager breadcrumbs and actions

---
+++ apps/office/src/containers/fileManager/FileManagerActions.tsx
@@ -0,0 +1,53 @@
+import { useCallback, useState } from 'react';
+
+// import { Box } from '@mui/material';
+import Button from '@mui/material/Button';
+
+// import ToggleButton from '@mui/material/ToggleButton';
+// import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+
+import Iconify from '@devist/ui-react/components/Iconify';
+import useBoolean from '@devist/ui-react/hooks/useBoolean';
+
+import FileManagerNewFolderDialog from '@/office/components/file-manager/FileManagerNewFolderDialog';
+
+// import EmptyContent from '@/office/components/EmptyContent';
+// import Iconify from '@/ui-react/components/Iconify';
+
+const FileManagerActions = () => {
+	const upload = useBoolean();
+	const newFolder = useBoolean();
+	const [folderName, setFolderName] = useState('');
+
+	const handleChangeFolderName = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+		setFolderName(event.target.value);
+	}, []);
+
+	return (
+		<>
+			<Button variant="contained" startIcon={<Iconify icon="eva:cloud-upload-fill" />} onClick={newFolder.setTrue}>
+				New Folder
+			</Button>
+			<Button variant="contained" startIcon={<Iconify icon="eva:cloud-upload-fill" />} onClick={upload.setTrue}>
+				Upload
+			</Button>
+
+			<FileManagerNewFolderDialog open={upload.value} onClose={upload.setFalse} />
+
+			<FileManagerNewFolderDialog
+				open={newFolder.value}
+				onClose={newFolder.setFalse}
+				title="New Folder"
+				onCreate={() => {
+					newFolder.setFalse();
+					setFolderName('');
+					console.info('CREATE NEW FOLDER', folderName);
+				}}
+				folderName={folderName}
+				onChangeFolderName={handleChangeFolderName}
+			/>
+		</>
+	);
+};
+
+export default FileManagerActions;
--- apps/office/src/containers/fileManager/FileManagerBreadcrumbs.tsx
+++ apps/office/src/containers/fileManager/FileManagerBreadcrumbs.tsx
@@ -1,8 +1,18 @@
 import { useMemo, type ComponentProps } from 'react';
 
 import CustomBreadcrumbs from '@/office/components/CustomBreadcrumbs';
+import { BO_PATH_NAMES } from '@/shared/lib/constants';
 
 import useFileManager from './useFilManager';
+
+const getFileManagerBreadcrumbURL = (folderPath: string) => {
+	const url = new URL(window.location.origin);
+	url.pathname = BO_PATH_NAMES.fileManager;
+	const searchParams = new URLSearchParams();
+	searchParams.set('folderPath', folderPath);
+	url.search = decodeURIComponent(searchParams.toString());
+	return url.toString();
+};
 
 // --------------------
 const FileManagerBreadcrumbs = () => {
@@ -11,24 +21,23 @@ const FileManagerBreadcrumbs = () => {
 
 	//  = folderPath.split('/')
 	const links = useMemo(() => {
-		const iLinks: ComponentProps<typeof CustomBreadcrumbs>['links'] = [{ name: '/' }];
+		const iLinks: ComponentProps<typeof CustomBreadcrumbs>['links'] = [
+			{ name: '/', href: getFileManagerBreadcrumbURL('/') },
+		];
 
-		// if (folderPath === '/') {
-		// 	iLinks.push({ name: '/' });
-		// 	return iLinks;
-		// }
-
-		folderPath.split('/').forEach((name, index /* ,  array */) => {
+		folderPath.split('/').forEach((name, index, array) => {
 			if (index < 1) return;
+
 			iLinks.push({
 				name,
+				href: getFileManagerBreadcrumbURL(array.slice(0, index + 1).join('/')),
 			});
 		});
 
 		return iLinks;
 	}, [folderPath]);
 
-	return <CustomBreadcrumbs links={links} /* separator=">" */ sx={{ marginBottom: '22px' }} />;
+	return <CustomBreadcrumbs links={links} /* separator=">" */ /* sx={{ marginBottom: '22px' }} */ />;
 };
 
 export default FileManagerBreadcrumbs;
