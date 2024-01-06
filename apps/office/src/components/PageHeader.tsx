4c20a53c1b13a892397bcfc9dc88ec4df7ecce1e
Andrianarisoa Daniel <radandevist@gmail.com>
2024-01-06 07:32:45 +0300

    feat(front): add MDX and Yopta editor components with page header

---
+++ apps/office/src/components/PageHeader.tsx
@@ -0,0 +1,188 @@
+import type { ReactNode } from 'react';
+
+import Box from '@mui/material/Box';
+import Breadcrumbs, { type BreadcrumbsProps } from '@mui/material/Breadcrumbs';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import type { SxProps } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+
+import RouterLink from './RouterLink';
+
+// ----------------------------------------------------------------------
+
+export type BreadcrumbsLinkProps = {
+	name?: string;
+	href?: string;
+	icon?: React.ReactElement;
+};
+
+export interface PageHeaderProps {
+	heading?: ReactNode;
+	moreLink?: string[];
+	// activeLast?: boolean;
+	action?: ReactNode;
+	// links: BreadcrumbsLinkProps[];
+	sx?: SxProps;
+	breadcrumbs: ReactNode;
+}
+
+// ----------------------------------------------------------------------
+
+const Separator = () => {
+	return (
+		<Box
+			component="span"
+			sx={{
+				width: 4,
+				height: 4,
+				borderRadius: '50%',
+				bgcolor: 'text.disabled',
+			}}
+		/>
+	);
+};
+
+// ----------------------------------------------------------------------
+
+type LinkItemProps = {
+	link: BreadcrumbsLinkProps;
+	activeLast?: boolean;
+	disabled: boolean;
+};
+
+const LinkItem = ({ link, activeLast, disabled }: LinkItemProps) => {
+	const { name, href, icon } = link;
+
+	const styles = {
+		typography: 'body2',
+		alignItems: 'center',
+		color: 'text.primary',
+		display: 'inline-flex',
+		...(disabled &&
+			!activeLast && {
+				cursor: 'default',
+				pointerEvents: 'none',
+				color: 'text.disabled',
+			}),
+	};
+
+	const renderContent = (
+		<>
+			{icon && (
+				<Box
+					component="span"
+					sx={{
+						mr: 1,
+						display: 'inherit',
+						'& svg': { width: 20, height: 20 },
+					}}
+				>
+					{icon}
+				</Box>
+			)}
+
+			{name}
+		</>
+	);
+
+	if (href) {
+		return (
+			<Link component={RouterLink} href={href} sx={styles}>
+				{renderContent}
+			</Link>
+		);
+	}
+
+	return <Box sx={styles}> {renderContent} </Box>;
+};
+// ----------------------------------------------------------------------
+
+const PageHeader = ({
+	/* links, */ action,
+	heading,
+	moreLink,
+	/* activeLast, */ sx /* ...other  */,
+	breadcrumbs,
+}: PageHeaderProps) => {
+	// const lastLink = links[links.length - 1].name;
+
+	return (
+		<Box sx={{ ...sx }}>
+			<Stack direction="row" alignItems="center">
+				<Box sx={{ flexGrow: 1 }}>
+					{/* HEADING */}
+					{heading}
+					{/* {heading && (
+						<Typography variant="h4" gutterBottom>
+							{heading}
+						</Typography>
+					)} */}
+
+					{/* BREADCRUMBS */}
+					{breadcrumbs}
+					{/* {!!links.length && (
+						<Breadcrumbs separator={<Separator />} {...other}>
+							{links.map((link) => {
+								return (
+									<LinkItem
+										key={link.name || ''}
+										link={link}
+										activeLast={activeLast}
+										disabled={link.name === lastLink}
+									/>
+								);
+							})}
+						</Breadcrumbs>
+					)} */}
+				</Box>
+
+				{action && <Box sx={{ flexShrink: 0 }}> {action} </Box>}
+			</Stack>
+
+			{/* MORE LINK */}
+			{!!moreLink && (
+				<Box sx={{ mt: 2 }}>
+					{moreLink.map((href) => {
+						return (
+							<Link key={href} href={href} variant="body2" target="_blank" rel="noopener" sx={{ display: 'table' }}>
+								{href}
+							</Link>
+						);
+					})}
+				</Box>
+			)}
+		</Box>
+	);
+};
+
+type PageHeaderBreadcrumbsProps = BreadcrumbsProps & {
+	links: BreadcrumbsLinkProps[];
+	activeLast?: boolean;
+};
+
+PageHeader.Breadcrumbs = ({ links, activeLast, ...other }: PageHeaderBreadcrumbsProps) => {
+	const lastLink = links[links.length - 1].name;
+
+	return (
+		<Breadcrumbs separator={<Separator />} {...other}>
+			{links.map((link) => {
+				return <LinkItem key={link.name || ''} link={link} activeLast={activeLast} disabled={link.name === lastLink} />;
+			})}
+		</Breadcrumbs>
+	);
+};
+
+type PageHeaderHeadingProps = {
+	text: string;
+};
+
+PageHeader.Heading = ({ text }: PageHeaderHeadingProps) => {
+	return (
+		<Typography variant="h4" gutterBottom>
+			{text}
+		</Typography>
+	);
+};
+
+export default PageHeader;
--- apps/office/src/containers/posts/NewPost.tsx
+++ apps/office/src/containers/posts/NewPost.tsx
@@ -1,7 +1,32 @@
-import Editor from '@devist/ui-react/components/Editor';
+// import Editor from '@devist/ui-react/components/Editor';
+import { Button, Container } from '@mui/material';
+
+import MdxEditor from '@devist/ui-react/components/MdxEditor';
+
+import PageHeader from '@/office/components/PageHeader';
 
 const NewPost = () => {
-	return <Editor />;
+	// return <Editor />;
+	const headingElement = <PageHeader.Heading text="New post" />;
+	const breadcrumbsElement = <PageHeader.Breadcrumbs links={[{ name: 'ok' }]} />;
+	const renderActions = (
+		<>
+			<Button>preview</Button>
+			<Button variant="contained">save</Button>
+		</>
+	);
+
+	return (
+		<Container maxWidth={/* settings.themeStretch ? false :  */ 'lg'}>
+			<PageHeader
+				heading={headingElement}
+				breadcrumbs={breadcrumbsElement}
+				action={renderActions}
+				// moreLink={['#']}
+			/>
+			<MdxEditor />;
+		</Container>
+	);
 };
 
 export default NewPost;
