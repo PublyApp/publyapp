17bdcd8b407c31318bb1523a84267638c7602021
Andrianarisoa Daniel <radandevist@gmail.com>
2024-01-14 21:09:07 +0300

    improvement public routes

--- apps/office/src/routes/_publicRoutes.tsx
+++ apps/office/src/routes/_publicRoutes.tsx
@@ -3,7 +3,7 @@ import { Suspense } from 'react';
 import Button from '@mui/material/Button';
 import { QueryErrorResetBoundary } from '@tanstack/react-query';
 import { ErrorBoundary, type FallbackProps } from 'react-error-boundary';
-import { Navigate, type RouteObject } from 'react-router-dom';
+import { Outlet, type RouteObject } from 'react-router-dom';
 
 import { BO_PATH_NAMES } from '@/shared/lib/constants';
 import { AUTH_REQUIRED_ERROR_MSG } from '@/ui-react/lib/react-query/features/auth/auth.actions';
@@ -11,29 +11,18 @@ import { AUTH_REQUIRED_ERROR_MSG } from '@/ui-react/lib/react-query/features/aut
 import PublicOnly from '../components/PublicOnly';
 import SplashScreen from '../components/SplashScreen';
 import LogIn from '../containers/logIn/LogIn';
-import usePathname from '../hooks/usePathname';
 
 import { getLastPath } from './utils';
 
 const FallBackComponent = ({ error, resetErrorBoundary }: FallbackProps) => {
-	// Call resetErrorBoundary() to reset the error boundary and retry the render.
-	const pathname = usePathname();
-
 	if (error.message === AUTH_REQUIRED_ERROR_MSG) {
-		if (pathname !== BO_PATH_NAMES.auth.login) {
-			resetErrorBoundary();
-			return <Navigate to={BO_PATH_NAMES.auth.login} />;
-		}
-
-		// resetErrorBoundary();
-		return <LogIn />;
+		return <Outlet />;
 	}
 
 	return (
 		<div role="alert">
 			<h1>Something went wrong!!</h1>
 			<Button onClick={resetErrorBoundary}>Retry loading</Button>
-			{/* <p>Something went wrong:</p> */}
 			{/* <pre style={{ color: 'red' }}>{error.message}</pre> */}
 		</div>
 	);
@@ -60,9 +49,10 @@ export const publicRoutes: RouteObject[] = [
 			{
 				path: getLastPath(BO_PATH_NAMES.auth.login),
 				element: <LogIn />,
+				index: true,
 				// element: null,
 			},
-			{ path: 'random', element: <h1>Random Test</h1> },
+			{ path: getLastPath(BO_PATH_NAMES.auth.register), element: <h1>Register form</h1> },
 		],
 	},
 ];
