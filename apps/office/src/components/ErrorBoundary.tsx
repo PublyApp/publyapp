5a67d231de3078e0b1c4e2386f9796736af3c370
Andrianarisoa Daniel <radandevist@gmail.com>
2024-01-19 06:32:40 +0300

    feat(office): add error boundary and restructure dashboard routes

---
+++ apps/office/src/components/ErrorBoundary.tsx
@@ -0,0 +1,73 @@
+import { type ComponentType, type ReactNode } from 'react';
+
+import { /* QueryErrorResetBoundary, */ useQueryErrorResetBoundary } from '@tanstack/react-query';
+import { ErrorBoundary as ErrorBoundaryLib, type FallbackProps } from 'react-error-boundary';
+import { Outlet } from 'react-router-dom';
+
+const ErrorBoundary = ({
+	FallbackComponent,
+	children,
+}: {
+	FallbackComponent: ComponentType<FallbackProps>;
+	children?: ReactNode;
+}) => {
+	const { reset } = useQueryErrorResetBoundary();
+
+	return (
+		<ErrorBoundaryLib
+			FallbackComponent={FallbackComponent}
+			onReset={() => {
+				console.log('call');
+				reset();
+			}}
+		>
+			{/* eslint-disable-next-line @typescript-eslint/no-use-before-define */}
+			{/* <Composed /> */}
+			{children ?? <Outlet />}
+		</ErrorBoundaryLib>
+	);
+
+	// return (
+	// 	<QueryErrorResetBoundary>
+	// 		{({ reset }) => {
+	// 			return (
+	// 				<ErrorBoundaryLib FallbackComponent={FallbackComponent} onReset={reset}>
+	// 					{children ?? <Outlet />}
+	// 				</ErrorBoundaryLib>
+	// 			);
+	// 		}}
+	// 	</QueryErrorResetBoundary>
+	// );
+};
+
+export default ErrorBoundary;
+
+// const Composed = () => {
+// 	const location = useLocation();
+// 	const { resetBoundary } = useErrorBoundary();
+
+// 	// useEffect(() => {
+// 	// 	console.log(location.key);
+// 	// });
+
+// 	useEffect(() => {
+// 		console.log(location.key);
+// 		resetBoundary();
+// 	}, [resetBoundary, location.key]);
+
+// 	return null;
+// };
+
+// export const useResetErrorBoundaryEffect = (resetBoundary: (...args: any[]) => void) => {
+// 	const location = useLocation();
+// 	// const { resetBoundary } = useErrorBoundary();
+
+// 	// useEffect(() => {
+// 	// 	console.log(location.key);
+// 	// });
+
+// 	useEffect(() => {
+// 		console.log(location.key);
+// 		resetBoundary();
+// 	}, [resetBoundary, location.key]);
+// };
--- packages/ui-react/lib/react-query/queryClient.ts
+++ packages/ui-react/lib/react-query/queryClient.ts
@@ -1,31 +1,44 @@
-import { /* MutationCache, */ QueryClient } from '@tanstack/react-query';
+import { /* MutationCache,  QueryCache, */ QueryClient } from '@tanstack/react-query';
 
 import { ClientException } from '@/ui-react/exceptions/ClientException';
 
-const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
+// const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
+const fiveMinsInMs = 1000 * 60 * 5;
 
-export const createQueryClient = () => {
+type Options = {
+	env?: 'production' | 'development';
+};
+
+export const createQueryClient = ({ env = 'development' }: Options = {}) => {
 	const queryClient = new QueryClient({
 		defaultOptions: {
 			queries: {
-				// refetchOnWindowFocus: false,
-				// refetchOnReconnect: false,
-				// retry: false
+				refetchOnWindowFocus: false,
+				refetchOnReconnect: false,
+				staleTime: fiveMinsInMs,
 				retry: (failureCount, error) => {
+					if (env === 'development') {
+						return false;
+					}
+
 					if (error instanceof ClientException) {
 						if (error.code === ClientException.AUTH_REQUIRED) {
 							return false;
 						}
 					}
 
-					return failureCount <= 3;
+					return failureCount <= 2;
 				},
-				staleTime: twentyFourHoursInMs,
 			},
 		},
+		// queryCache: new QueryCache({
+		// 	onError: (error, query) => {
+		// 		enqueueSnackbar(error);
+		// 	},
+		// }),
 		// mutationCache: new MutationCache({
 		// 	onError(error, variables, context, mutation) {
-		// 		toast(error);
+		// 		enqueueSnackbar(error);
 		// 	},
 		// })
 	});
