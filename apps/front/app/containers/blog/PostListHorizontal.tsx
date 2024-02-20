20a877f2fa1b3b00396017edaedf6aa088e66cdd
Andrianarisoa Daniel <radandevist@gmail.com>
2024-02-20 13:43:05 +0300

    wip: bo:  update post

--- apps/front/app/containers/blog/PostListHorizontal.tsx
+++ apps/front/app/containers/blog/PostListHorizontal.tsx
@@ -29,6 +29,7 @@ const PostListHorizontal = (/* { posts, loading }: Props */) => {
 	// const loading = useFakeLoading();
 	const loading = false;
 	const { posts } = useLoaderData<PostListLoaderFunction>();
+	// console.log('🤢🤢🤢🤢', posts);
 
 	const renderSkeleton = (
 		<>
