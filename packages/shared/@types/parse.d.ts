274097992134eaffee3be25488933f268f33a84b
Andrianarisoa Daniel <radandevist@gmail.com>
2024-02-27 14:02:27 +0300

    small improvement

--- packages/shared/@types/parse.d.ts
+++ packages/shared/@types/parse.d.ts
@@ -1,59 +1,57 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import type { DateType } from '../types/db/any.types';
+namespace Parse {
+	import type { PipelineStage } from 'mongoose';
 
-declare global {
-	namespace Parse {
-		import type { PipelineStage } from 'mongoose';
+	import type { DateType } from '../types/db/any.types';
 
-		interface BaseAttributes {
-			objectId: string;
-			createdAt?: DateType;
-			updatedAt?: DateType;
-		}
+	interface BaseAttributes {
+		objectId: string;
+		createdAt?: DateType;
+		updatedAt?: DateType;
+	}
 
-		export type PipelineStage = PipelineStage;
+	export type PipelineStage = PipelineStage;
 
-		export type OmitBaseAttributes<T> = Omit<T, keyof BaseAttributes>;
+	export type OmitBaseAttributes<T> = Omit<T, keyof BaseAttributes>;
 
-		namespace Cloud {
-			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			interface FunctionRequest<T extends Params = Params> {
-				headers: Record<string, any> | undefined;
-				ip: string | undefined;
-			}
-
-			// eslint-disable-next-line @typescript-eslint/ban-types
-			interface TriggerRequest<T = Object> {
-				query: Query<T> | undefined;
-				context: Record<string, unknown> | undefined;
-			}
-		}
-
-		interface InstallationController {
-			currentInstallationId(): Promise<string>;
-		}
-
-		namespace CoreManager {
-			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			function set(key: string, value: any): void;
-			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			function get(key: string): void;
-
-			function getInstallationController(): InstallationController;
+	namespace Cloud {
+		interface FunctionRequest<T extends Params = Params> {
+			headers: Record<string, any> | undefined;
+			ip: string | undefined;
 		}
 
 		// eslint-disable-next-line @typescript-eslint/ban-types
-		interface Query<T extends Object = Object> /* extends Parse.Query */ {
-			aggregate<V = any>(
-				pipeline: /* Query.AggregationOptions | Query.AggregationOptions[] */ PipelineStage[],
-			): Promise<V>;
+		interface TriggerRequest<T = Object> {
+			query: Query<T> | undefined;
+			context: Record<string, unknown> | undefined;
 		}
+	}
 
-		namespace Query {
-			interface FindOptions {
-				context?: Record<string, any>;
-			}
+	interface InstallationController {
+		currentInstallationId(): Promise<string>;
+	}
+
+	namespace CoreManager {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		function set(key: string, value: any): void;
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		function get(key: string): void;
+
+		function getInstallationController(): InstallationController;
+	}
+
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	interface Query<T extends Object = Object> /* extends Parse.Query */ {
+		aggregate<V = any>(
+			pipeline: /* Query.AggregationOptions | Query.AggregationOptions[] */ PipelineStage[],
+		): Promise<V>;
+	}
+
+	namespace Query {
+		interface FindOptions {
+			context?: Record<string, any>;
 		}
 	}
 }
