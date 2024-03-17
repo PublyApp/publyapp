5a0e576c6421a9b91d25fc07e23eef2f7e6cf334
Andrianarisoa Daniel <radandevist@gmail.com>
2024-03-17 11:34:46 +0300

    file upload adapters

---
+++ apps/server/src/resources/file/upload/CloudinaryUploadAdapter.ts
@@ -0,0 +1,17 @@
+import { sleep } from '@devist/shared/utils/any.utils';
+
+import type UploadAdapterInterface from './UploadAdapterInterface';
+import type { UploadInput } from './UploadAdapterInterface';
+
+export default class LocalDiskUploadAdapter implements UploadAdapterInterface {
+	provider = 'cloudinary' as const;
+
+	// eslint-disable-next-line class-methods-use-this
+	async upload(_params: UploadInput) {
+		await sleep(3000);
+
+		return {
+			url: 'fake-url-lol',
+		};
+	}
+}
