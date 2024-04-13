8a0518ce49938742054695caba846793cd12f994
Andrianarisoa Daniel <radandevist@gmail.com>
2024-04-13 14:52:51 +0300

    wip idk

---
+++ apps/server/scripts/patch.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable import/no-extraneous-dependencies */
+import replace from 'replace-in-file';
+
+console.log('==================> postInstall <=================');
+
+const path = require('path');
+
+const patchParseServerSelectNestedObjects = async () => {
+	const filePath1 = path.resolve(import.meta.dir, '../node_modules/parse-server/lib/RestQuery.js');
+	const filePath2 = path.resolve(import.meta.dir, '../../../node_modules/parse-server/lib/RestQuery.js');
+
+	const exists1 = Bun.file(filePath1).exists();
+
+	// const results =
+	replace({
+		disableGlobs: true,
+		files: (await exists1) ? filePath1 : filePath2,
+		from: /return key.split\('.'\)\[0\];/g,
+		to: 'return key;',
+	});
+};
+
+patchParseServerSelectNestedObjects();
