abe76cafaa9a248904d6472ec0eb3006a12d03f8
Andrianarisoa Daniel <radandevist@gmail.com>
2024-04-20 21:37:02 +0300

    refactor(api): wrap controllers with express handler wrapper

--- apps/server/src/middlewares/protection.middleware.ts
+++ apps/server/src/middlewares/protection.middleware.ts
@@ -9,6 +9,7 @@ import {
 	PARSE_SESSION_TOKEN_HEADER_KEY,
 } from '@/shared/lib/constants';
 
+import { expressHandler } from '../lib/express';
 import { getHeader } from '../utils/request.utils';
 
 type Input = {
@@ -18,57 +19,53 @@ type Input = {
 };
 
 const protectionMiddleware = ({ withKey = true, withAuth = true, withInstallation = false }: Input): RequestHandler => {
-	return async (req, _res, next) => {
-		try {
-			// should have a header key
-			if (withKey) {
-				const apiKey = getHeader(req, DEVIST_REST_API_HEADER_KEY);
+	return expressHandler(async (req, _res, next) => {
+		// should have a header key
+		if (withKey) {
+			const apiKey = getHeader(req, DEVIST_REST_API_HEADER_KEY);
 
-				// if the key exists, go to next
-				if (!apiKey) {
-					return next(new HttpException(400, `Missing ${DEVIST_REST_API_HEADER_KEY} param`));
-				}
-
-				if (apiKey && apiKey !== env.REST_API_KEY) {
-					return next(new HttpException(400, `Invalid ${DEVIST_REST_API_HEADER_KEY} param`));
-				}
+			// if the key exists, go to next
+			if (!apiKey) {
+				return next(new HttpException(400, `Missing ${DEVIST_REST_API_HEADER_KEY} param`));
 			}
 
-			// should have a header session token
-			if (withAuth) {
-				const sessionToken = getHeader(req, PARSE_SESSION_TOKEN_HEADER_KEY);
-
-				if (!sessionToken) {
-					return next(new HttpException(400, `Missing ${PARSE_SESSION_TOKEN_HEADER_KEY} param`));
-				}
-
-				const authService = await AuthCloudService.createAuthCloudService({ sessionToken });
-
-				const user = authService.getUserForSessionToken();
-
-				// if user exists, go to next
-				if (!(await user)) {
-					return next(new HttpException(400, 'User not found'));
-				}
-
-				req.user = await user;
+			if (apiKey && apiKey !== env.REST_API_KEY) {
+				return next(new HttpException(400, `Invalid ${DEVIST_REST_API_HEADER_KEY} param`));
 			}
-
-			if (withInstallation) {
-				const installationId = getHeader(req, PARSE_INSTALLATION_ID_HEADER_KEY);
-
-				if (!installationId) {
-					return next(new HttpException(400, `Missing ${PARSE_INSTALLATION_ID_HEADER_KEY} param`));
-				}
-
-				req.installationId = installationId;
-			}
-
-			return next();
-		} catch (error) {
-			return next(error);
 		}
-	};
+
+		// should have a header session token
+		if (withAuth) {
+			const sessionToken = getHeader(req, PARSE_SESSION_TOKEN_HEADER_KEY);
+
+			if (!sessionToken) {
+				return next(new HttpException(400, `Missing ${PARSE_SESSION_TOKEN_HEADER_KEY} param`));
+			}
+
+			const authService = await AuthCloudService.createAuthCloudService({ sessionToken });
+
+			const user = authService.getUserForSessionToken();
+
+			// if user exists, go to next
+			if (!(await user)) {
+				return next(new HttpException(400, 'User not found'));
+			}
+
+			req.user = await user;
+		}
+
+		if (withInstallation) {
+			const installationId = getHeader(req, PARSE_INSTALLATION_ID_HEADER_KEY);
+
+			if (!installationId) {
+				return next(new HttpException(400, `Missing ${PARSE_INSTALLATION_ID_HEADER_KEY} param`));
+			}
+
+			req.installationId = installationId;
+		}
+
+		return next();
+	});
 };
 
 export default protectionMiddleware;
