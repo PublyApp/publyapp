fc20cbaddb027b27900f6b4f7d89d48caf7375ea
Andrianarisoa Daniel <radandevist@gmail.com>
2024-04-20 16:21:16 +0300

    small improvement

--- packages/shared/utils/any.utils.ts
+++ packages/shared/utils/any.utils.ts
@@ -4,3 +4,7 @@ export const sleep = (timeout: number) => {
 		return setTimeout(resolve, timeout);
 	});
 };
+
+export const isAsyncFunction = (func: GenericFunction) => {
+	return func.constructor.name === 'AsyncFunction';
+};
