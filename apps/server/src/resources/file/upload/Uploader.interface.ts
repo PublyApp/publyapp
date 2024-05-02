5716981f40a49b01ef33d95844957e01c95af57e
Andrianarisoa Daniel <radandevist@gmail.com>
2024-05-02 20:58:42 +0300

    feat(api): scaffold social media post service and facebook platform

--- apps/server/src/resources/file/upload/Uploader.interface.ts
+++ apps/server/src/resources/file/upload/Uploader.interface.ts
@@ -2,9 +2,9 @@ export type UploadInput = { name: string; buffer: Buffer };
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type UploadResult = { url: string; meta?: Record<string, any> };
 
-interface UploaderInterface {
+interface Uploader {
 	readonly provider: string;
 	upload(params: UploadInput): Promise<UploadResult>;
 }
 
-export default UploaderInterface;
+export default Uploader;
