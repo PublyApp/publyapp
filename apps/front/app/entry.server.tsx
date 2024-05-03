ba4c2b42f140a959bfb826d0746bbb7f1b604e30
Andrianarisoa Daniel <radandevist@gmail.com>
2024-05-03 09:49:33 +0300

    wip

--- apps/front/app/entry.server.tsx
+++ apps/front/app/entry.server.tsx
@@ -1,29 +1,52 @@
+// import path from 'path';
+
 import { CacheProvider } from '@emotion/react';
 import createEmotionServer from '@emotion/server/create-instance';
 import type { EntryContext } from '@remix-run/node';
 import { RemixServer } from '@remix-run/react';
+import { createInstance } from 'i18next';
+import Backend from 'i18next-fs-backend';
 import * as ReactDOMServer from 'react-dom/server';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
 
 import ThemeProvider from '@devist/ui-react/providers/ThemeProvider';
 
 import createEmotionCache from './lib/emotion/createEmotionCache';
+import i18n from './lib/i18n/i18n';
+import i18next from './lib/i18n/i18next.server';
 
-const handleRequest = (
+const handleRequest = async (
 	request: Request,
 	responseStatusCode: number,
 	responseHeaders: Headers,
 	remixContext: EntryContext,
 ) => {
+	const instance = createInstance();
+	const lng = await i18next.getLocale(request);
+	const ns = i18next.getRouteNamespaces(remixContext);
+
+	await instance
+		.use(initReactI18next) // Tell our instance to use react-i18next
+		.use(Backend) // Setup our backend
+		.init({
+			...i18n, // spread the configuration
+			lng, // The locale we detected above
+			ns, // The namespaces the routes about to render wants to use
+			// backend: { loadPath: path.resolve('./public/locales/{{lng}}/{{ns}}.json') }, // ! i use Ts files so I don't need this
+		});
+
 	const cache = createEmotionCache();
 	const { extractCriticalToChunks } = createEmotionServer(cache);
 
 	const MuiRemixServer = () => {
 		return (
-			<CacheProvider value={cache}>
-				<ThemeProvider>
-					<RemixServer context={remixContext} url={request.url} />
-				</ThemeProvider>
-			</CacheProvider>
+			<I18nextProvider i18n={instance}>
+				<CacheProvider value={cache}>
+					<ThemeProvider>
+						<RemixServer context={remixContext} url={request.url} />
+					</ThemeProvider>
+				</CacheProvider>
+			</I18nextProvider>
 		);
 	};
 
