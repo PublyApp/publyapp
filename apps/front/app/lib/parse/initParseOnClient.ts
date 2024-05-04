62b1f5d5554314f9a3a620354b044063986cfc8e
Andrianarisoa Daniel <radandevist@gmail.com>
2024-05-04 20:03:24 +0300

    wip

---
+++ apps/front/app/lib/parse/initParseOnClient.ts
@@ -0,0 +1,18 @@
+import { LOCALE_HEADER_KEY } from '@/shared/lib/constants';
+import { defaultLocale } from '@/shared/lib/i18n/resources';
+import parseApi from '@/ui-react/api/parse/ParseApi';
+
+import { returnLanguageIfSupported } from '../i18n/i18n';
+
+import { parseRestClient } from './client';
+
+export const initParseOnClient = () => {
+	// get local from url
+	const localeInUrl: string | undefined = window.location.pathname.split('/')[1];
+	const locale = returnLanguageIfSupported(localeInUrl) ?? defaultLocale;
+
+	// set locale header
+	parseRestClient.setHeader(LOCALE_HEADER_KEY, locale);
+
+	parseApi.setRestClient(parseRestClient);
+};
