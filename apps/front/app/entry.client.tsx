dc03aec77da285991326fa3fd5ecbb9ed8cf6f7e
Andrianarisoa Daniel <radandevist@gmail.com>
2024-05-04 20:03:24 +0300

    refactor(front): extract client-side i18next and parse initializers

--- apps/front/app/entry.client.tsx
+++ apps/front/app/entry.client.tsx
@@ -3,21 +3,15 @@ import * as React from 'react';
 import { CacheProvider } from '@emotion/react';
 import { RemixBrowser } from '@remix-run/react';
 import i18next from 'i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import * as ReactDOM from 'react-dom/client';
-import { I18nextProvider, initReactI18next } from 'react-i18next';
-import { getInitialNamespaces } from 'remix-i18next/client';
+import { I18nextProvider } from 'react-i18next';
 
 import ThemeProvider from '@devist/ui-react/providers/ThemeProvider';
 
-import { LOCALE_HEADER_KEY } from '@/shared/lib/constants';
-import { defaultLocale } from '@/shared/lib/i18n/resources';
-import parseApi from '@/ui-react/api/parse/ParseApi';
-
 import ClientStyleContext from './contexts/ClientStyleContext';
 import createEmotionCache from './lib/emotion/createEmotionCache';
-import i18n, { returnLanguageIfSupported } from './lib/i18n/i18n';
-import { parseRestClient } from './lib/parse/client';
+import { initI18nextOnClient } from './lib/i18n/initI18nextOnClient';
+import { initParseOnClient } from './lib/parse/initParseOnClient';
 
 // import theme from './src/theme';
 
@@ -64,9 +58,7 @@ const hydrate = async () => {
 	// 		},
 	// 	});
 
-	// eslint-disable-next-line @typescript-eslint/no-use-before-define
 	initI18nextOnClient();
-	// eslint-disable-next-line @typescript-eslint/no-use-before-define
 	initParseOnClient();
 	// TODO: init the libs below
 	// initNumeral();
@@ -93,36 +85,3 @@ if (window.requestIdleCallback) {
 	// https://caniuse.com/requestidlecallback
 	window.setTimeout(hydrate, 1);
 }
-
-const initI18nextOnClient = () => {
-	i18next
-		.use(LanguageDetector) // Setup a client-side language detector
-		.use(initReactI18next) // Tell i18next to use the react-i18next plugin
-		// .use(Backend) // Setup your backend
-		.init({
-			...i18n, // spread the configuration
-			// This function detects the namespaces your routes rendered while SSR use
-			ns: getInitialNamespaces(),
-			// backend: { loadPath: '/locales/{{lng}}/{{ns}}.json' }, // ! I don't need http-backend for now
-			detection: {
-				// Here only enable htmlTag detection, we'll detect the language only
-				// server-side with remix-i18next, by using the `<html lang>` attribute
-				// we can communicate to the client the language detected server-side
-				order: ['htmlTag'],
-				// Because we only use htmlTag, there's no reason to cache the language
-				// on the browser, so we disable it
-				caches: [],
-			},
-		});
-};
-
-const initParseOnClient = () => {
-	// get local from url
-	const localeInUrl: string | undefined = window.location.pathname.split('/')[1];
-	const locale = returnLanguageIfSupported(localeInUrl) ?? defaultLocale;
-
-	// set locale header
-	parseRestClient.setHeader(LOCALE_HEADER_KEY, locale);
-
-	parseApi.setRestClient(parseRestClient);
-};
