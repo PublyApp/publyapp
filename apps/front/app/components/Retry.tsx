b29ce1977ce064e29955770939a44a7cebd9e3a5
Andrianarisoa Daniel <radandevist@gmail.com>
2024-05-13 19:17:53 +0300

    refactor(front): refine retry component and post content loading

--- apps/front/app/components/Retry.tsx
+++ apps/front/app/components/Retry.tsx
@@ -1,18 +1,19 @@
 import { type MouseEventHandler } from 'react';
 
 import { LoadingButton } from '@mui/lab';
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, type ContainerProps } from '@mui/material';
 
 import Iconify from '@/ui-react/components/Iconify';
 import useTranslate from '@/ui-react/hooks/useTranslate';
 
 type Props = {
 	message: string;
-	onRetry: MouseEventHandler;
+	onRetry?: MouseEventHandler;
 	loading?: boolean;
-};
+	hideRetryButton?: boolean;
+} & ContainerProps;
 
-const Retry = ({ message, onRetry, loading }: Props) => {
+const Retry = ({ message, onRetry, loading, sx, hideRetryButton, children, ...other }: Props) => {
 	const { t } = useTranslate();
 
 	return (
@@ -24,26 +25,28 @@ const Retry = ({ message, onRetry, loading }: Props) => {
 				borderBottom: (theme) => {
 					return `solid 1px ${theme.palette.divider}`;
 				},
+				...sx,
 			}}
+			{...other}
 		>
 			<Typography variant="h3" mb={3}>
 				{message}
 			</Typography>
 			{/* <p>{description}</p> */}
-			<LoadingButton
-				size="large"
-				variant="contained"
-				// <Icon icon="fa6-solid:arrow-rotate-right" />
-				// <Icon icon="gravity-ui:arrow-rotate-right" />
-				startIcon={<Iconify icon="gravity-ui:arrow-rotate-right" width={24} />}
-				// startIcon={<Iconify icon="svg-spinners:12-dots-scale-rotate" width={24} />}
-				loadingIndicator={<Iconify icon="svg-spinners:12-dots-scale-rotate" width={24} />}
-				loading={loading}
-				loadingPosition="start"
-				onClick={onRetry}
-			>
-				<span>{t('retry')}</span>
-			</LoadingButton>
+			{!hideRetryButton ? (
+				<LoadingButton
+					size="large"
+					variant="contained"
+					startIcon={<Iconify icon="gravity-ui:arrow-rotate-right" width={24} />}
+					loadingIndicator={<Iconify icon="svg-spinners:12-dots-scale-rotate" width={24} />}
+					loading={loading}
+					loadingPosition="start"
+					onClick={onRetry}
+				>
+					<span>{t('retry')}</span>
+				</LoadingButton>
+			) : null}
+			{children}
 		</Container>
 	);
 };
