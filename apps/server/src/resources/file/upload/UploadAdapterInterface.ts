9642c6511a27ebaa735a2449a8e57daf79064d39
Andrianarisoa Daniel <radandevist@gmail.com>
2024-05-26 10:23:44 +0300

    group resources by feature modules

--- apps/server/src/resources/file/upload/UploadAdapterInterface.ts
+++
@@ -1,9 +0,0 @@
-export type UploadInput = { name: string; buffer: Buffer };
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type UploadResult = { url: string; meta?: Record<string, any> };
-interface UploadAdapterInterface {
-	readonly provider: string;
-	upload(params: UploadInput): Promise<UploadResult>;
-}
-
-export default UploadAdapterInterface;
