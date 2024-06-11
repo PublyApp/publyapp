8261f174dffb7ca17786c4b7ec22f8a822c0f37a
Andrianarisoa Daniel <radandevist@gmail.com>
2024-06-11 21:31:35 +0300

    refactor(office): rename RequireAuth to AuthGuard and prune db types

--- apps/office/src/components/AuthGuard.tsx
+++ apps/office/src/components/AuthGuard.tsx
@@ -1,23 +1,42 @@
-import type { ReactNode } from 'react';
+import { useMemo, type ReactNode } from 'react';
 
 import { /*  Navigate, */ Outlet /* , useLocation */ } from 'react-router-dom';
 
 // import { BO_PATH_NAMES } from '@devist/shared/lib/constants';
 import { useGetClientAuthSuspenseQuery } from '@devist/ui-react/lib/react-query/features/auth/auth.hooks';
 
+import type { IRoleConfig } from '@/shared/lib/constants';
+
 type Props = {
 	children?: ReactNode;
+	allowedRoles?: IRoleConfig[];
 };
 
-const RequireAuth = ({ children }: Props) => {
+const RequireAuth = ({ children, allowedRoles }: Props) => {
 	// const location = useLocation();
 	const {
-		result: { /* data: authData */ error },
+		result: { data: authData, error },
 	} = useGetClientAuthSuspenseQuery();
 
 	if (error) {
 		// return null;
 		throw error;
+	}
+
+	const hasRequiredRoles = useMemo(() => {
+		if (!allowedRoles) {
+			return true;
+		}
+
+		return authData.roles.some((role) => {
+			return allowedRoles.some((allowedRole) => {
+				return allowedRole.code === role.code;
+			});
+		});
+	}, [allowedRoles, authData.roles]);
+
+	if (!hasRequiredRoles) {
+		return <h1>Page 403: not allowed (todo: better look)</h1>;
 	}
 
 	return children ?? <Outlet />;
