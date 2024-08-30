8da8b908c57c52b0b0bfde98ca4b87d16db21990
Andrianarisoa Daniel <radandevist@gmail.com>
2024-08-30 09:53:13 +0300

    feat(tenant): add tenant-aware parse query and object classes

--- apps/server/src/cloud.ts
+++ apps/server/src/cloud.ts
@@ -1,8 +1,43 @@
 /* eslint-disable @typescript-eslint/no-use-before-define */
+// import { nanoid } from 'nanoid';
+
 import logger from './lib/logger';
+
+// import TenantQuery from './lib/parse/classes/TenantQuery';
 
 export const cloud = async () => {
 	try {
+		// Parse.Cloud.define('testContextInBeforeFind-create', async (req) => {
+		// 	// const a = new Parse.Object('Dummy', { attr1: 'test' });
+		// 	// a.id = 'TGboiCeYDs';
+
+		// 	// const objs = ['TGboiCeYDs', 'l8wFQ8Rsar'].map((id) => {
+		// 	// 	return new Parse.Object('Dummy', { objectId: id, attr1: nanoid(4) });
+		// 	// });
+		// 	const objs = await new /* Parse.Query */ TenantQuery({
+		// 		className: 'Dummy',
+		// 		tenantId: 'ok',
+		// 	}).findAll(/* { context: { tenantId: 'ok' } } */);
+
+		// 	// objs.forEach((obj) => {
+		// 	// 	obj.set('attr1', nanoid(4));
+		// 	// });
+
+		// 	// const newObjs = await Parse.Object.saveAll(objs);
+
+		// 	// const obj1 = newObjs[0];
+		// 	// obj1.set('attr1', nanoid(2));
+		// 	// await obj1.save();
+		// });
+
+		// Parse.Cloud.beforeFind('Dummy', (req) => {
+		// 	console.log('⭕⭕⭕⭕', req.query instanceof Parse.Query);
+		// });
+
+		// Parse.Cloud.beforeSave('Dummy', (req) => {
+		// 	console.log('⭕⭕⭕⭕');
+		// });
+
 		await Promise.all([functions(), triggers(), jobs()]);
 	} catch (error) {
 		logger.error('Error while importing cloud code:', error);
