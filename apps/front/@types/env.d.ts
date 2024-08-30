15fcd742c928d7b55765af37db1522c772907ab0
Andrianarisoa Daniel <radandevist@gmail.com>
2024-08-30 05:05:33 +0300

    improve shared declarations files

--- apps/front/@types/env.d.ts
+++ apps/front/@types/env.d.ts
@@ -1,11 +1,6 @@
 /// <reference types="@remix-run/node" />
 /// <reference types="vite/client" />
 
-/// <reference types="@emotion/react/types/css-prop" />
-/// <reference types="vite-plugin-svgr/client" />
-
-/// <reference types="@devist/ui-react/@types/@mui__material" />
-/// <reference types="@devist/ui-react/@types/@tanstack__react-table" />
-/// <reference types="@devist/ui-react/@types/@emotion__react" />
+/// <reference types="@devist/ui-react/@types/index" />
 
 /// <reference types="@devist/shared/@types/index" />
