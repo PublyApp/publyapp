1ad92eda72b83179adc43c5c65abfba0b4123d11
Andrianarisoa Daniel <radandevist@gmail.com>
2024-09-14 06:29:34 +0300

    relocate users-tenants relation

--- packages/shared/types/db/tenant.types.ts
+++ packages/shared/types/db/tenant.types.ts
@@ -1,11 +1,23 @@
 import type { BaseAttributes } from 'parse';
 
+import type ParseUser from '@/server/resources/auth/user/user.class';
+
+import type { IUser } from './user.types';
+
 export type TenantAttributes = {
 	name: string;
 };
 
 export type ITenant = BaseAttributes & TenantAttributes;
 
-export type ITenantWithRelations = ITenant /* & {} */;
+export type ITenantWithRelations = ITenant & {
+	users: {
+		user: IUser;
+	}[];
+};
 
-export type ITenantWithParseRelations = ITenant /* & {} */;
+export type ITenantWithParseRelations = ITenant & {
+	users: {
+		user: ParseUser;
+	}[];
+};
