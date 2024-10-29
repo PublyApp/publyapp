3e08f275e1e00c752024a6bd9ec1041e80fa5dad
Andrianarisoa Daniel <radandevist@gmail.com>
2024-10-29 11:26:24 +0300

    refactor(shared): extend CustomZod and align zod usages

--- apps/server/src/lib/zod.ts
+++ apps/server/src/lib/zod.ts
@@ -1,6 +1,5 @@
-import { defaultLocale } from '@/shared/lib/i18n/resources';
 import CustomZod from '@/shared/lib/zod/CustomZod';
 
-import { getT } from './i18n';
+import { i18nextServer } from './i18n';
 
-export const defaultZodServer = new CustomZod(getT(defaultLocale));
+export const defaultZodServer = new CustomZod({ i18n: i18nextServer });
