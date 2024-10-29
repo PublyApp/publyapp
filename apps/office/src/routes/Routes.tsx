e3032c4ec570932527278b79a3214acc2c7ff9f0
Andrianarisoa Daniel <radandevist@gmail.com>
2024-10-29 08:58:01 +0300

    move to nuqs for url state management

--- apps/office/src/routes/Routes.tsx
+++ apps/office/src/routes/Routes.tsx
@@ -1,18 +1,20 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { env } from '../lib/env';
-import QueryParamProvider from '../providers/QueryParamProvider';
+
+// import QueryParamProvider from '../providers/QueryParamProvider';
 
 import { authedRoutes } from './authed/AuthedRoutes';
 import { publicRoutes } from './public/PublicRoutes';
 
 const router = createBrowserRouter(
-	[
-		{
-			element: <QueryParamProvider />,
-			children: [...authedRoutes, ...publicRoutes],
-		},
-	],
+	// [
+	// 	{
+	// 		element: <QueryParamProvider />,
+	// 		children: [...authedRoutes, ...publicRoutes],
+	// 	},
+	// ],
+	[...authedRoutes, ...publicRoutes],
 	{
 		basename: env.OFFICE_ROUTER_BASENAME,
 	},
