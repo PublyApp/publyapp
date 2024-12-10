ad3d5fb8e4c742ae02904e4c30214379fa5c1c8a
Andrianarisoa Daniel <radandevist@gmail.com>
2024-12-10 21:30:29 +0300

    wip

--- packages/ui-react/@types/index.d.ts
+++ packages/ui-react/@types/index.d.ts
@@ -1,8 +1,6 @@
 /// <reference types="@emotion/react/types/css-prop" />
 /// <reference types="vite-plugin-svgr/client" />
 
-/// <reference types="./@tanstack__react-table" />
-
 // for fixing date-fns directory import
 declare module 'date-fns/locale/index.js' {
 	export * from 'date-fns/locale';
