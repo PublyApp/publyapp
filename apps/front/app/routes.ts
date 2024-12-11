cb67f260127d96390fba27f5791f44f676fd8670
Andrianarisoa Daniel <radandevist@gmail.com>
2024-12-11 07:56:59 +0300

    wip

--- apps/front/app/routes.ts
+++ apps/front/app/routes.ts
@@ -1,4 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { index, type RouteConfig } from '@react-router/dev/routes';
 
-export default [index('routes/home.tsx')] satisfies RouteConfig;
+export default [index('routes/marketing/Home.tsx')] satisfies RouteConfig;
--- apps/front/app/routes/marketing/home.tsx
+++ apps/front/app/routes/marketing/home.tsx
@@ -1,4 +1,4 @@
-import type { Route } from './+types/home';
+import type { Route } from './+types/Home';
 
 export const meta = (_: Route.MetaArgs) => {
 	return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }];
