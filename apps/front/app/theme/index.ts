b8ab08b87ef9bc55cb635fbb7c7351583964e6e3
Andrianarisoa Daniel <radandevist@gmail.com>
2024-12-11 07:56:59 +0300

    wip

---
+++ apps/front/app/theme/index.ts
@@ -0,0 +1,13 @@
+// theme.ts
+import { createTheme } from '@mantine/core';
+import { themeToVars } from '@mantine/vanilla-extract';
+
+// Do not forget to pass theme to MantineProvider
+export const theme = createTheme({
+	// fontFamily: 'serif',
+	// primaryColor: 'cyan',
+});
+// theme.css.ts
+
+// CSS variables object, can be access in *.css.ts files
+export const vars = themeToVars(theme);
