441d1091c2e6d168a22942a2091f456338dd64ab
Andrianarisoa Daniel <radandevist@gmail.com>
2024-12-12 14:54:13 +0300

    refactor(front): simplify login page markup

--- apps/front/app/routes/auth/LoginPage.tsx
+++ apps/front/app/routes/auth/LoginPage.tsx
@@ -1,8 +1,5 @@
-import { useEffect } from 'react';
-
 import { Box, Button, PasswordInput, TextInput } from '@mantine/core';
-import _ from 'lodash';
-import { Form } from 'react-router';
+import { data, Form } from 'react-router';
 
 import type { Route } from './+types/LoginPage';
 
@@ -17,25 +14,21 @@ export const action = async ({ request }: Route.ActionArgs) => {
 		password,
 	});
 
-	return {
+	return data({
 		// test: new Map([
 		// 	['email', _.toString(email)],
 		// 	['password', _.toString(password)],
 		// ]),
 		message: 'hello',
-	};
+	});
 };
 
 const LoginPage = ({ actionData }: Route.ComponentProps) => {
 	console.log('🙏🙏🙏🙏', actionData);
 
-	useEffect(() => {
-		console.log('😂😂');
-	});
-
 	return (
 		<Box>
-			<Form method="post" navigate={false}>
+			<Form method="post">
 				<TextInput label="Email" name="email" />
 				<PasswordInput label="Password" name="password" />
 				<Button type="submit" variant="primary">
