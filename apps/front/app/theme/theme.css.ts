c4552871875bfa2b8f3e08b410a8a57d8b6febfc
Andrianarisoa Daniel <radandevist@gmail.com>
2024-12-12 07:35:20 +0300

    wip

--- apps/front/app/theme/theme.css.ts
+++ apps/front/app/theme/theme.css.ts
@@ -3,8 +3,6 @@ import { themeToVars } from '@mantine/vanilla-extract';
 
 import { themeOptions } from './options';
 
-// import { themeOptions } from './theme';
-
 const theme = createTheme(themeOptions);
 
 // CSS variables object, can be access in *.css.ts files
