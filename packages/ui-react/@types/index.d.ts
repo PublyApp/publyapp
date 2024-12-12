512eaadcc3e4eb5efaa36964fac04e13f0904770
Andrianarisoa Daniel <radandevist@gmail.com>
2024-12-12 10:40:25 +0300

    feat(front): add class merging utility and remove ui-react package

--- packages/ui-react/@types/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="@emotion/react/types/css-prop" />
-/// <reference types="vite-plugin-svgr/client" />
-
-// for fixing date-fns directory import
-declare module 'date-fns/locale/index.js' {
-	export * from 'date-fns/locale';
-}
