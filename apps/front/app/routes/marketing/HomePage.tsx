1753895595eada81fe09e3c5ab93fbab01c5a572
Andrianarisoa Daniel <radandevist@gmail.com>
2024-12-12 15:58:30 +0300

    feat(front): prototype streamed loader data with suspense on home page

--- apps/front/app/routes/marketing/HomePage.tsx
+++ apps/front/app/routes/marketing/HomePage.tsx
@@ -1,4 +1,8 @@
-import { Button } from '@mantine/core';
+import React, { Suspense } from 'react';
+
+import { Box, Button } from '@mantine/core';
+
+import { sleep } from '@/shared/utils/any.utils';
 
 import type { Route } from './+types/HomePage';
 
@@ -6,11 +10,45 @@ export const meta = (_: Route.MetaArgs) => {
 	return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }];
 };
 
-const HomePage = () => {
+const fn1 = async () => {
+	return sleep(3000, 'cool');
+};
+
+const fn2 = async () => {
+	await sleep(3000);
+	throw new Error('Intentional error');
+};
+
+const fn3 = async () => {
+	return sleep(3000, 'very cool');
+};
+
+export const loader = async (_: Route.LoaderArgs) => {
+	const p1 = fn1();
+	const p2 = fn2();
+	const p3 = fn3();
+
+	return {
+		p1,
+		p2,
+		p3,
+	};
+};
+
+const DisplayData = ({ data }: { data: Promise<string> }) => {
+	const display = React.use(data);
+	return <Box>{display}</Box>;
+};
+
+const HomePage = ({ loaderData }: Route.ComponentProps) => {
+	console.log('🚀🚀🚀🚀', loaderData);
 	return (
 		<div>
 			<h1>Hello!!</h1>
 			<Button variant="primary">Hello</Button>
+			<Suspense fallback={<h3>Loading...</h3>}>
+				<DisplayData data={loaderData.p1} />
+			</Suspense>
 		</div>
 	);
 };
