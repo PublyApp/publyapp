7cfc50f32b2b27eae43c36717adcad57fd267f7d
Andrianarisoa Daniel <radandevist@gmail.com>
2024-12-13 08:23:47 +0300

    reveal server and client entrypoints

---
+++ apps/front/app/entry.server.tsx
@@ -0,0 +1,66 @@
+import { PassThrough } from 'node:stream';
+
+import { createReadableStreamFromReadable } from '@react-router/node';
+import { isbot } from 'isbot';
+import { renderToPipeableStream, type RenderToPipeableStreamOptions } from 'react-dom/server';
+import { ServerRouter, type AppLoadContext, type EntryContext } from 'react-router';
+
+const ABORT_DELAY = 5_000;
+
+const handleRequest = (
+	request: Request,
+	responseStatusCode: number,
+	responseHeaders: Headers,
+	routerContext: EntryContext,
+	_loadContext: AppLoadContext,
+) => {
+	return new Promise((resolve, reject) => {
+		let shellRendered = false;
+		const userAgent = request.headers.get('user-agent');
+
+		// Ensure requests from bots and SPA Mode renders wait for all content to load before responding
+		// https://react.dev/reference/react-dom/server/renderToPipeableStream#waiting-for-all-content-to-load-for-crawlers-and-static-generation
+		const readyOption: keyof RenderToPipeableStreamOptions =
+			(userAgent && isbot(userAgent)) || routerContext.isSpaMode ? 'onAllReady' : 'onShellReady';
+
+		const { pipe, abort } = renderToPipeableStream(
+			<ServerRouter context={routerContext} url={request.url} abortDelay={ABORT_DELAY} />,
+			{
+				[readyOption]: () => {
+					shellRendered = true;
+					const body = new PassThrough();
+					const stream = createReadableStreamFromReadable(body);
+
+					responseHeaders.set('Content-Type', 'text/html');
+
+					resolve(
+						new Response(stream, {
+							headers: responseHeaders,
+							status: responseStatusCode,
+						}),
+					);
+
+					pipe(body);
+				},
+				onShellError: (error: unknown) => {
+					reject(error);
+				},
+				onError: (error: unknown) => {
+					// eslint-disable-next-line no-param-reassign
+					responseStatusCode = 500;
+
+					// Log streaming rendering errors from inside the shell.  Don't log
+					// errors encountered during initial shell rendering since they'll
+					// reject and get logged in handleDocumentRequest.
+					if (shellRendered) {
+						console.error(error);
+					}
+				},
+			},
+		);
+
+		setTimeout(abort, ABORT_DELAY);
+	});
+};
+
+export default handleRequest;
