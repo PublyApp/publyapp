f097ba076f83a68754ed653dbf9e7a624a42205d
Andrianarisoa Daniel <radandevist@gmail.com>
2024-12-22 19:53:34 +0300

    i18n wip

---
+++ apps/front/app/components/LanguagePicker/LanguagePicker.css.ts
@@ -0,0 +1,31 @@
+import { style } from '@vanilla-extract/css';
+
+export const classes = {
+	control: style({
+		width: '200px',
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		padding: 'var(--mantine-spacing-xs) var(--mantine-spacing-md)',
+		borderRadius: 'var(--mantine-radius-md)',
+		border: '1px solid light-dark(var(--mantine-color-gray-2), var(--mantine-color-dark-6))',
+		transition: 'background-color 150ms ease',
+		backgroundColor: 'light-dark(var(--mantine-color-white), var(--mantine-color-dark-6))',
+		selectors: {
+			'&[data-expanded]': {
+				backgroundColor: 'light-dark(var(--mantine-color-gray-0), var(--mantine-color-dark-5))',
+			},
+		},
+		':hover': {
+			backgroundColor: 'light-dark(var(--mantine-color-gray-0), var(--mantine-color-dark-5))',
+		},
+	}),
+	label: style({ fontWeight: 500, fontSize: 'var(--mantine-font-size-sm)' }),
+	icon: style({
+		transition: 'transform 150ms ease',
+		transform: 'rotate(0deg)',
+		selectors: {
+			'[data-expanded] &': { transform: 'rotate(180deg)' },
+		},
+	}),
+};
--- apps/front/app/entry.client.tsx
+++ apps/front/app/entry.client.tsx
@@ -3,11 +3,25 @@ import { startTransition, StrictMode } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 import { HydratedRouter } from 'react-router/dom';
 
-startTransition(() => {
-	hydrateRoot(
-		document,
-		<StrictMode>
-			<HydratedRouter />
-		</StrictMode>,
-	);
-});
+import { initI18nOnClient } from './lib/i18n/init.client';
+
+const hydrate = async () => {
+	await initI18nOnClient();
+
+	startTransition(() => {
+		hydrateRoot(
+			document,
+			<StrictMode>
+				<HydratedRouter />
+			</StrictMode>,
+		);
+	});
+};
+
+if (window.requestIdleCallback) {
+	window.requestIdleCallback(hydrate);
+} else {
+	// Safari doesn't support requestIdleCallback
+	// https://caniuse.com/requestidlecallback
+	window.setTimeout(hydrate, 1);
+}
