a5a8a7e308c9d07a894591c3366ea7d17e0dd07e
Andrianarisoa Daniel <radandevist@gmail.com>
2025-01-05 16:59:05 +0300

    feat(tenant): add tenant dashboard routes and role triggers

---
+++ apps/front/app/routes/authed/client/TenantDashboardPagesLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router';
+
+const TenantDashboardPagesLayout = () => {
+	return (
+		<div>
+			TenantDashboardPagesLayout
+			<Outlet />
+		</div>
+	);
+};
+
+export default TenantDashboardPagesLayout;
