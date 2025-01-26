389974f78c60afd5ac8bbedca075ab6b22bf7da1
Andrianarisoa Daniel <radandevist@gmail.com>
2025-01-26 08:05:15 +0300

    refactor(server): rename custom logger adapter to WinstonLoggerAdapter

--- apps/front/app/routes/authed/admin/AdminDashboardPagesLayout.tsx
+++ apps/front/app/routes/authed/admin/AdminDashboardPagesLayout.tsx
@@ -1,12 +1,39 @@
+import { AppShell, Burger, Group, Skeleton, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { nanoid } from 'nanoid';
 import { Outlet } from 'react-router';
 
-const AdminDashboardLayout = () => {
+const AdminDashboardPagesLayout = () => {
+	const [opened, { toggle }] = useDisclosure();
+
 	return (
-		<div>
-			<h1>AdminDashboardLayout</h1>
-			<Outlet />
-		</div>
+		<AppShell
+			layout="alt"
+			header={{ height: 60 }}
+			navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened, desktop: !opened } }}
+			padding="md"
+		>
+			<AppShell.Header>
+				<Group h="100%" px="md">
+					<Burger opened={opened} onClick={toggle} size="sm" />
+				</Group>
+			</AppShell.Header>
+			<AppShell.Navbar p="md">
+				<Group>
+					<Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+					<Text>Navbar</Text>
+				</Group>
+				{Array(15)
+					.fill(0)
+					.map((_) => {
+						return <Skeleton key={nanoid()} h={28} mt="sm" animate={false} />;
+					})}
+			</AppShell.Navbar>
+			<AppShell.Main>
+				<Outlet />
+			</AppShell.Main>
+		</AppShell>
 	);
 };
 
-export default AdminDashboardLayout;
+export default AdminDashboardPagesLayout;
