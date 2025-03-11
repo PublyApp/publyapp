d9f7c8cded1e9a7ae0b70e158d1caaf5f60ac9cf
Andrianarisoa Daniel <radandevist@gmail.com>
2025-03-11 08:11:25 +0300

    small improvement

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -2,10 +2,11 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+	plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), checker({ typescript: true })],
 	server: {
 		port: 6181,
 	},
