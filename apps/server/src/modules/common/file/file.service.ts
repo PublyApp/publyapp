17b71910960ab2e9e4103376465bb55c4bbd3a4e
Andrianarisoa Daniel <radandevist@gmail.com>
2025-03-24 14:02:32 +0300

    refactor(file-manager): move uploader interfaces and services to common module
    Restructure the file upload functionality by relocating the Uploader interface and related services from the tenant-specific module to the common module. This improves code reusability and maintainability across different parts of the application. The changes include:
    - Moving the Uploader interface to the common module with enhanced type support.
    - Introducing LocalDiskUploader and CloudinaryUploader implementations in the common module.
    - Creating a new FileService class to handle file upload operations with configurable upload adapters.

---
+++ apps/server/src/modules/common/file/file.service.ts
@@ -0,0 +1,68 @@
+import async from 'async';
+
+import CloudinaryUploader from './upload/CloudinaryUploader';
+import LocalDiskUploader from './upload/LocalDiskUploader';
+import { type Uploader } from './upload/Uploader.interface';
+
+export type FileServiceProps = {
+	sessionToken: string | undefined;
+	uploadAdapter: Uploader;
+};
+
+export default class AppFileService {
+	sessionToken?: string;
+
+	uploadAdapter: Uploader;
+
+	constructor({ sessionToken, uploadAdapter }: FileServiceProps) {
+		this.sessionToken = sessionToken;
+		this.uploadAdapter = uploadAdapter;
+	}
+
+	static isImage(file: Express.Multer.File | string) {
+		if (typeof file === 'string') {
+			return file.startsWith('image/');
+		}
+
+		return file.mimetype.startsWith('image/');
+	}
+
+	async uploadOne({ file, folderPath }: { file: Express.Multer.File; folderPath?: string }) {
+		const filename = file.originalname;
+
+		// upload the file here
+		const result = await this.uploadAdapter.upload({
+			buffer: file.buffer,
+			name: filename,
+			folderPath,
+		});
+
+		return result;
+	}
+
+	async uploadMany({ files, folderPath }: { files: Express.Multer.File[]; folderPath?: string }) {
+		const results = await async.map(files, async (file: Express.Multer.File) => {
+			const savedFile = await this.uploadOne({ file, folderPath });
+			return savedFile;
+		});
+
+		return results;
+	}
+
+	public static get uploadAdapterMap() {
+		const localDiskUploader = new LocalDiskUploader();
+		const cloudinaryUploader = new CloudinaryUploader();
+
+		const uploadAdapterMap = new Map([
+			[localDiskUploader.provider, localDiskUploader as Uploader],
+			[cloudinaryUploader.provider, cloudinaryUploader as Uploader],
+		]);
+
+		return uploadAdapterMap;
+	}
+
+	public static get defaultUploadAdapter(): Uploader {
+		// return new LocalDiskUploader();
+		return new CloudinaryUploader();
+	}
+}
