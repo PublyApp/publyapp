85eaebac436b7561ab6825ee2e98d44f835af8a2
Andrianarisoa Daniel <radandevist@gmail.com>
2025-03-25 10:52:08 +0300

    chore: remove unused REST API key and add cross-env dependency
    The REST API key (`VITE_REST_API_KEY`) was removed from the environment variables and related schema as it is no longer needed. Additionally, `cross-env` was added as a dependency to ensure environment variable consistency across different operating systems when starting the server.

--- apps/front/app/lib/env.ts
+++ apps/front/app/lib/env.ts
@@ -6,14 +6,12 @@ import { deepFreeze } from '@/shared/utils/any.utils';
 
 const envSchema = z.object({
 	VITE_SERVER_URL: z.string(),
-	VITE_REST_API_KEY: z.string(),
 });
 
 type AppEnv = z.infer<typeof envSchema>;
 
 const dotEnv = {
 	VITE_SERVER_URL: import.meta.env.VITE_SERVER_URL,
-	VITE_REST_API_KEY: import.meta.env.VITE_REST_API_KEY,
 } satisfies Partial<AppEnv>;
 
 export const env = deepFreeze(envSchema.parse(dotEnv));
