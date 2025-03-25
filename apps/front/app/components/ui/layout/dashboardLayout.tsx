e8bba931470ca674597db7fa26b2ce83cb72f7db
Andrianarisoa Daniel <radandevist@gmail.com>
2025-03-25 20:24:02 +0300

    refactor(layout): extract MainContainer component for dynamic width adjustment
    The MainContainer component was extracted from DashboardLayout to handle dynamic width adjustments based on the sidebar state. This improves maintainability and readability by separating concerns.
    
    Additionally, the TenantsTable component was updated to include more detailed mock data and enable row actions for editing and deleting tenants. This enhances the table's functionality and provides a more comprehensive user experience.

--- apps/front/app/components/ui/layout/dashboardLayout.tsx
+++ apps/front/app/components/ui/layout/dashboardLayout.tsx
@@ -1,26 +1,47 @@
 import type { ReactNode } from 'react';
 
-import { SidebarProvider, SidebarTrigger } from '@/front/components/tremor/Sidebar';
+import { SidebarProvider, SidebarTrigger, useSidebar } from '@/front/components/tremor/Sidebar';
 import { AppSidebar } from '@/front/components/ui/navigation/AppSidebar';
 import { Breadcrumbs } from '@/front/components/ui/navigation/Breadcrumbs';
+
+import { cx } from '../../tremor/tremor.utils';
 
 type Props = {
 	children?: ReactNode;
 	defaultOpenSideBar?: boolean;
 };
 
+type MainContainerProps = {
+	children?: ReactNode;
+};
+
+const MainContainer = ({ children }: MainContainerProps) => {
+	const { open } = useSidebar();
+
+	return (
+		<div
+			className={cx({
+				'w-[calc(100%-var(--sidebar-width))]': open,
+				'w-full': !open,
+			})}
+		>
+			{children}
+		</div>
+	);
+};
+
 const DashboardLayout = ({ children, defaultOpenSideBar }: Props) => {
 	return (
 		<SidebarProvider defaultOpen={defaultOpenSideBar}>
 			<AppSidebar />
-			<div className="w-full">
+			<MainContainer>
 				<header className="sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 border-b border-gray-200 bg-white px-4 dark:border-gray-800 dark:bg-gray-950">
 					<SidebarTrigger className="-ml-1" />
 					<div className="mr-2 h-4 w-px bg-gray-200 dark:bg-gray-800" />
 					<Breadcrumbs />
 				</header>
 				<main>{children}</main>
-			</div>
+			</MainContainer>
 		</SidebarProvider>
 	);
 };
