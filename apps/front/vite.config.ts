98dcdf5c67b75a8e8052c19b30cf995a154309df
Andrianarisoa Daniel <radandevist@gmail.com>
2025-03-28 09:52:46 +0300

    build: add Pigment CSS plugin and update dependencies
    Integrate Pigment CSS into the Vite configuration and update the HomePage component to utilize Pigment CSS for styling. Additionally, adjust package.json to include the necessary Pigment CSS dependencies.

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import { pigment } from '@pigment-css/vite-plugin';
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
@@ -6,7 +7,7 @@ import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), checker({ typescript: true })],
+	plugins: [pigment({}), tailwindcss(), reactRouter(), tsconfigPaths(), checker({ typescript: true })],
 	server: {
 		port: 6181,
 	},
