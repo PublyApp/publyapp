54bcab94d7ec3e06c6912f2f56a5f833e4c002bf
Andrianarisoa Daniel <radandevist@gmail.com>
2025-03-28 09:52:46 +0300

    build: add Pigment CSS plugin and update dependencies
    Integrate Pigment CSS into the Vite configuration and update the HomePage component to utilize Pigment CSS for styling. Additionally, adjust package.json to include the necessary Pigment CSS dependencies.

--- apps/front/app/routes/marketing/HomePage.tsx
+++ apps/front/app/routes/marketing/HomePage.tsx
@@ -1,6 +1,8 @@
+import { css } from '@pigment-css/react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router';
 
+import { cx } from '@/front/components/tremor/tremor.utils';
 import { FRONT_PATH_NAMES } from '@/shared/lib/constants';
 
 import type { Route } from './+types/HomePage';
@@ -9,13 +11,19 @@ export const meta = (_: Route.MetaArgs) => {
 	return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }];
 };
 
+const styles = css({
+	'& .test': {
+		color: 'red',
+	},
+});
+
 const HomePage = ({ loaderData: _ }: Route.ComponentProps) => {
 	const { t } = useTranslation();
 
 	return (
-		<div>
+		<div className={cx(styles)}>
 			<h1>{t('hello')}!!</h1>
-			<h2>The product is coming soon!</h2>
+			<h2 className="test">The product is coming soon!</h2>
 			<Link to={{ pathname: FRONT_PATH_NAMES.auth.login }} className="text-blue-500 underline">
 				Log in
 			</Link>
