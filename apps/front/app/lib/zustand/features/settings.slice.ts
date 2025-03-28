65428547946454b9a9c1d090f23bd70f571f9989
Andrianarisoa Daniel <radandevist@gmail.com>
2025-03-28 20:30:49 +0300

    feat(zustand): add zustand store with slices and middleware
    This commit introduces a new state management system using zustand. It includes the creation of a store, slices for dummy and settings, and middleware for persistence and devtools integration. The zustand package has also been added to the project dependencies.

---
+++ apps/front/app/lib/zustand/features/settings.slice.ts
@@ -0,0 +1,91 @@
+import _ from 'lodash';
+import type { Dispatch, SetStateAction } from 'react';
+
+// import type { IPostWithRelations, TranslatedIPostWithRelations } from '@/shared/types/db/post.types';
+
+import type { RootState } from '../slices';
+import Slice from '../utils/Slice';
+
+export type SettingsSliceValues = {
+	// edit post page
+	// currentlyEditedPost: IPostWithRelations | undefined;
+
+	// posts list (table)
+	// posts: TranslatedIPostWithRelations[];
+	// selectedPosts: TranslatedIPostWithRelations[];
+
+	sideBar: 'mini' | 'large';
+	isOpenNav: boolean;
+};
+
+export type SettingsSliceActions = {
+	setSidebar: Dispatch<SetStateAction<SettingsSliceValues['sideBar']>>;
+	setIsOpenNav: Dispatch<SetStateAction<SettingsSliceValues['isOpenNav']>>;
+};
+
+export type SettingsSliceState = SettingsSliceValues & SettingsSliceActions;
+
+const defaultValues: SettingsSliceValues = {
+	sideBar: 'large',
+	isOpenNav: true,
+};
+
+const sliceName = 'settingsSlice' as const;
+
+const settingsSlice = new Slice<SettingsSliceState, typeof sliceName>({
+	name: sliceName,
+	defaultValues,
+	initializer: (set) => {
+		return {
+			...defaultValues,
+
+			setSidebar: (value) => {
+				set((state) => {
+					let newValue: SettingsSliceValues['sideBar'];
+
+					if (_.isFunction(value)) {
+						newValue = value(state.settingsSlice.sideBar);
+					} else {
+						newValue = value;
+					}
+
+					// eslint-disable-next-line no-param-reassign
+					state.settingsSlice.sideBar = newValue;
+				});
+			},
+			setIsOpenNav: (value) => {
+				set((state) => {
+					let newValue: SettingsSliceValues['isOpenNav'];
+
+					if (_.isFunction(value)) {
+						newValue = value(state.settingsSlice.isOpenNav);
+					} else {
+						newValue = value;
+					}
+
+					// eslint-disable-next-line no-param-reassign
+					state.settingsSlice.isOpenNav = newValue;
+				});
+			},
+		};
+	},
+});
+
+export default settingsSlice;
+
+// ---- selectors ------------------------------------------------------------------------
+export const selectSidebar = (state: RootState) => {
+	return state.settingsSlice.sideBar;
+};
+
+export const selectSetSidebar = (state: RootState) => {
+	return state.settingsSlice.setSidebar;
+};
+
+export const selectIsOpenNav = (state: RootState) => {
+	return state.settingsSlice.isOpenNav;
+};
+
+export const selectSetIsOpenNav = (state: RootState) => {
+	return state.settingsSlice.setIsOpenNav;
+};
