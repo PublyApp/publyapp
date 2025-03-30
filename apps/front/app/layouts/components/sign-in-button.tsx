75f8a8c12583c38b0ac48551fcbc027e1aca3dce
Andrianarisoa Daniel <radandevist@gmail.com>
2025-03-30 15:17:20 +0300

    chore: add new assets and update button target attributes
    Added new icons, images, and illustrations to the public assets directory. Removed the `target="_blank"` attribute from buttons in the main layout and mobile navigation to improve user experience by keeping navigation within the same tab.

--- apps/front/app/layouts/components/sign-in-button.tsx
+++ apps/front/app/layouts/components/sign-in-button.tsx
@@ -3,10 +3,6 @@ import Button, { type ButtonProps } from '@mui/material/Button';
 import { RouterLink } from '@/front/components/router-link';
 import { FRONT_PATH_NAMES } from '@/shared/lib/constants';
 
-// import { RouterLink } from 'src/routes/components';
-
-// import { CONFIG } from 'src/global-config';
-
 // ----------------------------------------------------------------------
 
 export const SignInButton = ({ sx, ...other }: ButtonProps) => {
