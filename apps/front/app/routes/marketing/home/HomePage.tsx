f05d842018c7d663fb80872b32e5a3c302bf45d9
Andrianarisoa Daniel <radandevist@gmail.com>
2025-03-30 06:47:11 +0300

    refactor: remove unused components and dependencies to streamline codebase

--- apps/front/app/routes/marketing/home/HomePage.tsx
+++ apps/front/app/routes/marketing/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import type { Route } from '../+types/HomePage';
+import type { Route } from './+types/HomePage';
 
 export const meta = (_: Route.MetaArgs) => {
 	return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }];
