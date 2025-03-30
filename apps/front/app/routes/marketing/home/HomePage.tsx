ffdf103349ca3b6b6c4e374e8ea7490b32e013b2
Andrianarisoa Daniel <radandevist@gmail.com>
2025-03-30 15:17:20 +0300

    chore: add new assets and update button target attributes
    Added new icons, images, and illustrations to the public assets directory. Removed the `target="_blank"` attribute from buttons in the main layout and mobile navigation to improve user experience by keeping navigation within the same tab.

--- apps/front/app/routes/marketing/home/HomePage.tsx
+++ apps/front/app/routes/marketing/home/HomePage.tsx
@@ -1,14 +1,33 @@
+import { BackToTopButton } from '@/front/components/animate/back-to-top-button';
+import { ScrollProgress } from '@/front/components/animate/scroll-progress/scroll-progress';
+import { useScrollProgress } from '@/front/components/animate/scroll-progress/use-scroll-progress';
+
 import type { Route } from './+types/HomePage';
+import { HomeHero } from './parts/home-hero';
 
 export const meta = (_: Route.MetaArgs) => {
 	return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }];
 };
 
 const HomePage = ({ loaderData: _ }: Route.ComponentProps) => {
+	const pageProgress = useScrollProgress();
+
 	return (
-		<div>
-			<h1>🏠🏠🏠 Home page</h1>
-		</div>
+		<>
+			<ScrollProgress
+				variant="linear"
+				progress={pageProgress.scrollYProgress}
+				sx={[
+					(theme) => {
+						return { position: 'fixed', zIndex: theme.zIndex.appBar + 1 };
+					},
+				]}
+			/>
+
+			<BackToTopButton />
+
+			<HomeHero />
+		</>
 	);
 };
 
