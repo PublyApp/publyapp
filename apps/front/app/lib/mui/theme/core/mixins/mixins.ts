3c079fac968b6839c4c830bea16abb7af1b2bcf6
Andrianarisoa Daniel <radandevist@gmail.com>
2025-03-30 08:10:11 +0300

    feat: add MUI theme configuration and components

---
+++ apps/front/app/lib/mui/theme/core/mixins/mixins.ts
@@ -0,0 +1,52 @@
+import type { CSSObject, MixinsOptions } from '@mui/material/styles';
+
+import { bgBlur, bgGradient } from './background';
+import { borderGradient } from './border';
+import { menuItemStyles, paperStyles } from './global-styles-components';
+import { maxLine, textGradient } from './text';
+
+// ----------------------------------------------------------------------
+
+export type * from './text';
+export type * from './border';
+export type * from './background';
+export type * from './global-styles-components';
+
+export type MixinsExtend = {
+	hideScrollX: CSSObject;
+	hideScrollY: CSSObject;
+	bgBlur: typeof bgBlur;
+	maxLine: typeof maxLine;
+	bgGradient: typeof bgGradient;
+	paperStyles: typeof paperStyles;
+	textGradient: typeof textGradient;
+	borderGradient: typeof borderGradient;
+	menuItemStyles: typeof menuItemStyles;
+};
+
+/**
+ * TypeScript (type definition and extension)
+ * @to {@link file://./../../extend-theme-types.d.ts}
+ */
+
+export const mixins: MixinsOptions = {
+	hideScrollX: {
+		msOverflowStyle: 'none',
+		scrollbarWidth: 'none',
+		overflowX: 'auto',
+		'&::-webkit-scrollbar': { display: 'none' },
+	},
+	hideScrollY: {
+		msOverflowStyle: 'none',
+		scrollbarWidth: 'none',
+		overflowY: 'auto',
+		'&::-webkit-scrollbar': { display: 'none' },
+	},
+	borderGradient,
+	bgGradient,
+	bgBlur,
+	textGradient,
+	paperStyles,
+	menuItemStyles,
+	maxLine,
+};
