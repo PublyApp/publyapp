6006d318dcfe0f21fc1e1fd9a550ee0bbf2c8f25
Andrianarisoa Daniel <radandevist@gmail.com>
2025-03-30 08:10:11 +0300

    feat: add MUI theme configuration and components

---
+++ apps/front/app/lib/mui/theme/create-theme.ts
@@ -0,0 +1,60 @@
+import { createTheme as createMuiTheme, type Components, type Theme } from '@mui/material/styles';
+
+import { components } from './core/components';
+import { customShadows } from './core/custom-shadows';
+import { mixins } from './core/mixins';
+import { palette } from './core/palette';
+import { shadows } from './core/shadows';
+import { typography } from './core/typography';
+import { themeConfig } from './theme-config';
+import type { ThemeOptions } from './types';
+import { updateComponentsWithSettings, updateCoreWithSettings } from './with-settings';
+
+// ----------------------------------------------------------------------
+
+export const baseTheme: ThemeOptions = {
+	colorSchemes: {
+		light: {
+			palette: palette.light,
+			shadows: shadows.light,
+			customShadows: customShadows.light,
+		},
+		dark: {
+			palette: palette.dark,
+			shadows: shadows.dark,
+			customShadows: customShadows.dark,
+		},
+	},
+	mixins,
+	components,
+	typography,
+	shape: { borderRadius: 8 },
+	direction: themeConfig.direction,
+	cssVariables: themeConfig.cssVariables,
+	defaultColorScheme: themeConfig.defaultMode,
+};
+
+// ----------------------------------------------------------------------
+
+type CreateThemeProps = {
+	settingsState?: any; // SettingsState; // TODO: fix type later
+	themeOverrides?: ThemeOptions;
+	localeComponents?: { components?: Components<Theme> };
+};
+
+export const createTheme = ({
+	settingsState,
+	themeOverrides = {},
+	localeComponents = {},
+}: CreateThemeProps = {}): Theme => {
+	// Update core theme settings
+	const updatedCore = settingsState ? updateCoreWithSettings(baseTheme, settingsState) : baseTheme;
+
+	// Update component settings
+	const updatedComponents = settingsState ? updateComponentsWithSettings(components, settingsState) : {};
+
+	// Create and return the final theme
+	const theme = createMuiTheme(updatedCore, updatedComponents, localeComponents, themeOverrides);
+
+	return theme;
+};
---
+++ apps/front/app/lib/mui/theme/with-settings/right-to-left.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import rtlPlugin from 'stylis-plugin-rtl';
+
+import type { ThemeDirection } from '../types';
+
+// ----------------------------------------------------------------------
+
+type RtlProps = {
+	children: React.ReactNode;
+	direction: ThemeDirection;
+};
+
+const cacheRtl = createCache({
+	key: 'rtl',
+	prepend: true,
+	stylisPlugins: [rtlPlugin],
+});
+
+export function Rtl({ children, direction }: RtlProps) {
+	useEffect(() => {
+		document.dir = direction;
+	}, [direction]);
+
+	if (direction === 'rtl') {
+		return <CacheProvider value={cacheRtl}>{children}</CacheProvider>;
+	}
+
+	return <>{children}</>;
+}
