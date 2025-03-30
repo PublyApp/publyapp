822e8091dd92dea37d4620f6734522bf39504974
Andrianarisoa Daniel <radandevist@gmail.com>
2025-03-30 09:38:20 +0300

    refactor: remove unused useIsMobile hook and adjust vite config

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -6,20 +6,29 @@ import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	plugins: [pigment({}), reactRouter(), tsconfigPaths(), checker({ typescript: true })],
+	plugins: [
+		// ====
+		pigment({}),
+		reactRouter(),
+		tsconfigPaths(),
+		checker({ typescript: true }),
+	],
 	server: {
 		port: 6181,
 	},
 	ssr: {
-		noExternal: [
-			'@mui/system',
-			'@mui/material',
-			'@mui/x-date-pickers',
-			'@mui/utils',
-			'@mui/x-data-grid',
-			'@mui/x-tree-view',
-			'@mui/x-internals',
-			'@mui/styled-engine',
-		],
+		noExternal:
+			process.env.NODE_ENV === 'production'
+				? [
+						'@mui/system',
+						'@mui/material',
+						'@mui/x-date-pickers',
+						'@mui/utils',
+						'@mui/x-data-grid',
+						'@mui/x-tree-view',
+						'@mui/x-internals',
+						'@mui/styled-engine',
+					]
+				: [],
 	},
 });
