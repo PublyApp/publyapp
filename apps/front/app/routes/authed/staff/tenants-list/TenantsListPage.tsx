d7de5da33b1595ff775111eab5b71c013549fc66
Andrianarisoa Daniel <radandevist@gmail.com>
2025-04-05 18:13:27 +0300

    refactor(staff): enhance layout and tenants list page with MUI components
    Improve the layout structure by integrating DashboardLayout and QuerySuspenseBoundary for better error handling and loading states. Update the TenantsListPage with MUI components for a more consistent and visually appealing design. These changes aim to enhance maintainability and user experience.

--- apps/front/app/routes/authed/staff/tenants-list/TenantsListPage.tsx
+++ apps/front/app/routes/authed/staff/tenants-list/TenantsListPage.tsx
@@ -1,8 +1,35 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { varAlpha } from 'minimal-shared/utils';
+
+import { DashboardContent } from '@/front/layouts/dashboard/content';
+
 const TenantsListPage = () => {
+	const renderContent = () => {
+		return (
+			<Box
+				sx={[
+					(theme) => {
+						return {
+							mt: 5,
+							width: 1,
+							height: 320,
+							borderRadius: 2,
+							border: `dashed 1px ${theme.vars.palette.divider}`,
+							bgcolor: varAlpha(theme.vars.palette.grey['500Channel'], 0.04),
+						};
+					},
+					// ...(Array.isArray(sx) ? sx : [sx]),
+				]}
+			/>
+		);
+	};
+
 	return (
-		<div>
-			<h1>Tenants</h1>
-		</div>
+		<DashboardContent maxWidth="xl">
+			<Typography variant="h4">Tenants</Typography>
+			{renderContent()}
+		</DashboardContent>
 	);
 };
 
