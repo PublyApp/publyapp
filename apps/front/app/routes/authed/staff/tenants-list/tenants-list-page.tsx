e2c1e9d9b5a18189c0e27207b695d6e17a9ffca4
Andrianarisoa Daniel <radandevist@gmail.com>
2025-04-13 11:38:14 +0300

    feat(custom-breadcrumbs): add custom breadcrumbs component with styles and links
    Introduce a new custom breadcrumbs component to enhance navigation in the application. The component includes styles, back links, more links, and breadcrumb links. Additionally, update the tenants list page to utilize the new breadcrumbs component for better navigation and user experience.

--- apps/front/app/routes/authed/staff/tenants-list/tenants-list-page.tsx
+++ apps/front/app/routes/authed/staff/tenants-list/tenants-list-page.tsx
@@ -1,12 +1,41 @@
-import Typography from '@mui/material/Typography';
-
 import { DashboardContent } from '@/front/layouts/dashboard/content';
 import _ from 'lodash';
+import { FRONT_PATH_NAMES } from '@/shared/lib/constants';
+import { useTranslate } from '@/front/hooks/use-translate';
+import { CustomBreadcrumbs } from '@/front/components/custom-breadcrumbs/custom-breadcrumbs';
+import { RouterLink } from '@/front/components/router-link';
+import Button from '@mui/material/Button';
+import { Iconify } from '@/front/components/iconify/iconify';
 
 const TenantsListPage = () => {
+	const { t } = useTranslate();
 	return (
-		<DashboardContent maxWidth="xl">
-			<Typography variant="h4">Tenants</Typography>
+		<DashboardContent
+			/* maxWidth="xl" */
+			sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}
+		>
+			<CustomBreadcrumbs
+				heading={t('list-of-items', { items: t('tenants') })}
+				links={[
+					// { name: 'Dashboard', href: paths.dashboard.root },
+					{
+						name: _.capitalize(t('tenants')),
+						href: FRONT_PATH_NAMES.staff.tenants.root,
+					},
+					{ name: _.capitalize(t('list')) },
+				]}
+				action={
+					<Button
+						component={RouterLink}
+						href="#"
+						variant="contained"
+						startIcon={<Iconify icon="mingcute:add-line" />}
+					>
+						New product
+					</Button>
+				}
+				sx={{ mb: { xs: 3, md: 5 } }}
+			/>
 		</DashboardContent>
 	);
 };
