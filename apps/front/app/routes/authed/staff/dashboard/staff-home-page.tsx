23ebcb8f8f633fe5e3d149ea536f0198ea3b8fef
Andrianarisoa Daniel <radandevist@gmail.com>
2025-04-13 09:52:56 +0300

    refactor(front): clean up error handling and remove unused code
    Remove commented-out code and simplify error handling logic across components. Replace retry functionality with a full page reload in the 500 view. Streamline layout components by removing unnecessary query suspense boundaries and redundant imports. These changes improve code maintainability and readability.

--- apps/front/app/routes/authed/staff/dashboard/staff-home-page.tsx
+++ apps/front/app/routes/authed/staff/dashboard/staff-home-page.tsx
@@ -2,7 +2,7 @@ import { redirect } from 'react-router';
 
 import { FRONT_PATH_NAMES } from '@/shared/lib/constants';
 
-// * I don't se what to show on ana eventual dashboard home page, so for now, we redirect this to the tenants lis page
+// * I don't see what to show on an eventual dashboard home page, so for now, we redirect this to the tenants list page
 export const loader = () => {
 	return redirect(FRONT_PATH_NAMES.staff.tenants.root);
 };
