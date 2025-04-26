d568d70c87b92bb1d36867177f160e689e17ffcc
Andrianarisoa Daniel <radandevist@gmail.com>
2025-04-26 17:36:49 +0300

    feat(table): add MRT table presets and enhance staff members list page
    - Introduce MRT table presets for consistent table configurations
    - Add `useMRTTable` hook for reusable table logic
    - Enhance staff members list page with new columns and pagination
    - Update user status handling and improve UI with avatars and labels

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -1,25 +1,38 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import { pigment } from '@pigment-css/vite-plugin';
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { reactRouterDevTools } from 'react-router-devtools';
 
 export default defineConfig({
-	plugins: [pigment({}), reactRouter(), tsconfigPaths(), checker({ typescript: true })],
+	plugins: [
+		reactRouterDevTools(),
+		reactRouter(),
+		tsconfigPaths(),
+		checker({ typescript: true }),
+	],
 	server: {
 		port: 6181,
 	},
 	ssr: {
-		noExternal: [
-			'@mui/system',
-			'@mui/material',
-			'@mui/x-date-pickers',
-			'@mui/utils',
-			'@mui/x-data-grid',
-			'@mui/x-tree-view',
-			'@mui/x-internals',
-			'@mui/styled-engine',
-		],
+		noExternal:
+			process.env.NODE_ENV === 'production'
+				? [
+						'@mui/system',
+						'@mui/material',
+						'@mui/x-date-pickers',
+						'@mui/utils',
+						'@mui/x-data-grid',
+						'@mui/x-tree-view',
+						'@mui/x-internals',
+						'@mui/styled-engine',
+						// ====
+						'mui-one-time-password-input',
+						'@tiptap/extension-code-block-lowlight',
+					]
+				: [
+						'mui-one-time-password-input',
+						'@tiptap/extension-code-block-lowlight',
+					],
 	},
 });
