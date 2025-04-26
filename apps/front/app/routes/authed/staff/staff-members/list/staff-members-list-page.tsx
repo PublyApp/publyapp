11fb26c4a7baaef2102de8bb0d1f02c3baf85992
Andrianarisoa Daniel <radandevist@gmail.com>
2025-04-27 00:18:30 +0300

    refactor(router-link): handle server-side URL origin and decode pathnames
    Ensure proper URL handling for server-side rendering by using `viteOrigin` when `isServer` is true. Decode pathnames to handle encoded characters and remove unnecessary hash symbols to improve URL consistency.

--- apps/front/app/routes/authed/staff/staff-members/list/staff-members-list-page.tsx
+++ apps/front/app/routes/authed/staff/staff-members/list/staff-members-list-page.tsx
@@ -1,15 +1,45 @@
 import _ from 'lodash';
-
 import { CustomBreadcrumbs } from '@/front/components/custom-breadcrumbs/custom-breadcrumbs';
 import { Iconify } from '@/front/components/iconify/iconify';
 import { RouterLink } from '@/front/components/router-link';
 import { useTranslate } from '@/front/hooks/use-translate';
 import { DashboardContent } from '@/front/layouts/dashboard/content';
-import { FRONT_PATH_NAMES } from '@/shared/lib/constants';
-
+import { APP_NAME, FRONT_PATH_NAMES, isServer } from '@/shared/lib/constants';
 import Button from '@mui/material/Button';
-
 import StaffMembersTable from './parts/staff-members-table';
+import type { Route } from './+types/staff-members-list-page';
+import i18next, { type TFunction } from 'i18next';
+import { getServerLoader } from '@/front/lib/react-router/server.data';
+import { data } from 'react-router';
+import { remixI18NextServer } from '@/front/lib/i18n/i18n.server';
+
+export const meta = (args: Route.MetaArgs) => {
+	if (isServer) {
+		return _.get(args.data, 'meta', []);
+	}
+
+	const t: TFunction = i18next.t;
+
+	return [
+		{
+			title: `${_.capitalize(t('list-of-items', { items: t('staff-members') }))} | Staff Dashboard - ${APP_NAME}`,
+		},
+	];
+};
+
+export const loader = getServerLoader({
+	loader: async ({ locale }) => {
+		const t = await remixI18NextServer.getFixedT(locale);
+
+		return data({
+			meta: [
+				{
+					title: `${_.capitalize(t('list-of-items', { items: t('staff-members') }))} | Staff Dashboard - ${APP_NAME}`,
+				},
+			],
+		});
+	},
+});
 
 const StaffMembersListPage = () => {
 	const { t } = useTranslate();
