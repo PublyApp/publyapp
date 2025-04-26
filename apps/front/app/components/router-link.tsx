c8e085ba3d95d31231f915f62ccbc72313ff712a
Andrianarisoa Daniel <radandevist@gmail.com>
2025-04-26 22:34:52 +0300

    feat(router-link): enhance URL handling with language query param
    Added logic to dynamically append the current language as a query parameter to internal URLs. This ensures consistent language settings across navigation. External URLs remain unchanged.

--- apps/front/app/components/router-link.tsx
+++ apps/front/app/components/router-link.tsx
@@ -1,12 +1,81 @@
-import { Link, type LinkProps } from 'react-router';
+import _ from 'lodash';
+import { Link, type LinkProps, type To } from 'react-router';
+import { useTranslate } from '../hooks/use-translate';
+import { queryParamKey } from '@/shared/lib/constants';
 
 // ----------------------------------------------------------------------
 
 interface RouterLinkProps extends Omit<LinkProps, 'to'> {
-	href: string;
+	href: To;
 	ref?: React.RefObject<HTMLAnchorElement | null>;
 }
 
+const checkIsExternalUrl = (to: To): to is string => {
+	if (_.isObject(to)) {
+		return false;
+	}
+
+	// let hasProtocol = false;
+	let url: URL | undefined;
+
+	try {
+		url = new URL(to);
+		// hasProtocol = true;
+	} catch (e) {}
+	// const hasProtocol = ['http://', 'https://'].some((protocol) => {
+	// 	return _.startsWith(to, protocol);
+	// });
+
+	if (!url) {
+		return false;
+	}
+
+	if (window.location.origin === url.origin) {
+		return true;
+	}
+
+	return false;
+};
+
 export const RouterLink = ({ href, ref, ...other }: RouterLinkProps) => {
-	return <Link ref={ref} to={href} {...other} />;
+	const isExternalUrl = checkIsExternalUrl(href);
+	const { currentLang } = useTranslate();
+
+	let to = href;
+
+	if (isExternalUrl) {
+		// do nothing
+	} else {
+		if (!_.isString(href)) {
+			const searchParams = new URLSearchParams(href.search);
+			searchParams.set(queryParamKey.language, currentLang.value);
+			to = {
+				pathname: href.pathname,
+				hash: href.hash,
+				search: searchParams.toString(),
+			};
+		} else {
+			let url: URL | undefined;
+
+			try {
+				url = new URL(href);
+			} catch (e) {}
+
+			if (!url) {
+				const [pathname, search] = _.split(href, '?');
+				url = new URL(window.location.origin);
+				url.pathname = pathname;
+				url.search = search || '';
+			}
+
+			url.searchParams.set(queryParamKey.language, currentLang.value);
+
+			to = {
+				pathname: url.pathname,
+				search: url.search,
+			};
+		}
+	}
+
+	return <Link ref={ref} to={to} {...other} />;
 };
