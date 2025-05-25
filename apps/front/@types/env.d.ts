b35f6212b7c402aefecc9a09b5b987a75011db23
Andrianarisoa Daniel <radandevist@gmail.com>
2025-05-25 11:15:26 +0300

    chore: update dependencies and remove unused module declaration
    Updated dependencies including `react`, `react-dom`, `@types/react`, `@types/react-dom`, and `vite` to their latest versions. Removed the unused module declaration for `react-dom/server.browser` and adjusted the Vite configuration to resolve `react-dom/server` correctly in production mode.

--- apps/front/@types/env.d.ts
+++ apps/front/@types/env.d.ts
@@ -1,8 +1,3 @@
 /// <reference types="vite/client" />
 
 /// <reference types="@org/shared/@types/index" />
-
-// https://github.com/oven-sh/bun/issues/9949#issuecomment-2124041655
-declare module 'react-dom/server.browser' {
-	export * from 'react-dom/server';
-}
