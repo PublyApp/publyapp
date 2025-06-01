2b4b2dfe194329517bd28680c68ab90f003f6b19
Andrianarisoa Daniel <radandevist@gmail.com>
2025-06-01 20:05:46 +0300

    feat(settings): persist nav layout state in cookies
    Add cookie persistence for navigation layout state changes in settings. The changes include:
    - Adding a subscription to the main store to save navLayout changes to cookies
    - Updating the client loader to initialize state from cookies
    - Removing obsolete sidebar cookie handling code
    - Adding a comment to clarify cookie max age calculation

--- apps/front/app/lib/zustand/store.ts
+++ apps/front/app/lib/zustand/store.ts
@@ -1,12 +1,32 @@
 import _ from 'lodash';
-
+import * as cookie from 'cookie';
 import { create } from 'zustand';
-
 import { getInitialStore, type RootState } from './slices';
 import { combinedMiddlewares } from './utils/middleware';
+import { SIDEBAR_COOKIE_MAX_AGE, SIDEBAR_COOKIE_NAME } from '../constants';
+import { defaultSettings } from '@/front/components/settings';
 
 export const useMainStore = create<RootState>()(
 	combinedMiddlewares((...a) => {
 		return getInitialStore(...a);
 	}),
 );
+
+useMainStore.subscribe((rootState, prevRootState) => {
+	if (
+		rootState.settingsSlice.state.navLayout !==
+		prevRootState.settingsSlice.state.navLayout
+	) {
+		const sidebarCookie = cookie.serialize(
+			SIDEBAR_COOKIE_NAME,
+			rootState.settingsSlice.state.navLayout ||
+				(defaultSettings.navLayout as never),
+			{
+				maxAge: SIDEBAR_COOKIE_MAX_AGE,
+				path: '/',
+			},
+		);
+
+		document.cookie = sidebarCookie;
+	}
+});
