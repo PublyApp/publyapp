7e108fe328ee93abd15c170644f4c17a16c95878
Andrianarisoa Daniel <radandevist@gmail.com>
2025-06-13 21:36:13 +0300

    refactor(server): remove unused code and optimize logging
    - Remove deprecated hello function and console override
    - Clean up imports and organize code structure
    - Optimize logging to only show verbose info in local env

--- apps/server/src/lib/parse/classes/SchemaManager.ts
+++ apps/server/src/lib/parse/classes/SchemaManager.ts
@@ -1,11 +1,11 @@
-import _ from 'lodash';
-import type { Schema } from 'parse-server';
-import _MongoSchemaCollection from 'parse-server/lib/Adapters/Storage/Mongo/MongoSchemaCollection.js';
-import asyncJs from 'async';
-import { MongoServerError, type CreateIndexesOptions } from 'mongodb';
-import { className as _className, isBun } from '@org/shared/lib/constants';
 import { logger } from '@/server/lib/winston';
 import { tryCatchWrapper } from '@/shared/utils/try-catch.utils';
+import { className as _className, isBun } from '@org/shared/lib/constants';
+import asyncJs from 'async';
+import _ from 'lodash';
+import { type CreateIndexesOptions, MongoServerError } from 'mongodb';
+import type { Schema } from 'parse-server';
+import _MongoSchemaCollection from 'parse-server/lib/Adapters/Storage/Mongo/MongoSchemaCollection.js';
 import { DEFAULT_CLP } from '../../constants';
 import { getDatabase } from '../parse.utils';
 
