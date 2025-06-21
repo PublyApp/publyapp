e7d7073ecf92a788ecefb9b0c756b76a8d2e608a
Andrianarisoa Daniel <radandevist@gmail.com>
2025-06-21 14:32:10 +0300

    fix(constants): update cookie names and error handling for session and email verification
    - Modify SIDEBAR_COOKIE_NAME and SESSION_TOKEN_COOKIE_KEY to use a consistent naming convention.
    - Enhance error handling in various components to use specific error codes for invalid session and email verification tokens.
    - Add new translations for session expiration messages in English and French.

--- apps/server/src/modules/common/auth/auth.functions.ts
+++ apps/server/src/modules/common/auth/auth.functions.ts
@@ -285,7 +285,7 @@ const checkEmailVerificationToken = fromPublicParseFunction({
 
 		if (!user) {
 			throw new HttpException(400, t('Invalid token'), {
-				xcode: X_CODE.INVALID_TOKEN,
+				xcode: X_CODE.INVALID_EMAIL_VERIFICATION_TOKEN,
 			});
 		}
 
@@ -294,7 +294,7 @@ const checkEmailVerificationToken = fromPublicParseFunction({
 
 		if (isExpired) {
 			const error = new HttpException(400, t('Invalid token'), {
-				xcode: X_CODE.INVALID_TOKEN,
+				xcode: X_CODE.INVALID_EMAIL_VERIFICATION_TOKEN,
 			});
 			// add additional information to the error
 			_.set(error, 'meta.cause', 'TOKEN_EXPIRED'); // only for debugging purposes, not to be exposed to the client
