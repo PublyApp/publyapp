61263ae2d7e8d8fc990c117e0146b6d495b6d76c
Andrianarisoa Daniel <radandevist@gmail.com>
2025-06-22 13:43:02 +0300

    feat(parse): implement fetchAttributesOnObjects utility for batch attribute retrieval
    - Introduce a new utility function `fetchAttributesOnObjects` to efficiently fetch missing attributes for an array of Parse objects.
    - Enhance error handling to ensure objects have valid IDs and belong to the same class.
    - Update role assignment methods in `RoleService` and related triggers to support multiple users, improving role management.
    - Add new validation logic in `StaffTenantService` to prevent assigning roles to users already associated with tenants, enhancing data integrity.
    - Update localization files to include new error messages related to tenant membership validation.

---
+++ apps/server/src/lib/parse/parse-object.ts
@@ -0,0 +1,102 @@
+import _ from 'lodash';
+import type { PipelineStage } from 'mongoose';
+import { getDatabase } from './parse.utils';
+
+export const fetchAttributesOnObjects = async <
+	T extends Parse.Object = Parse.Object,
+>(
+	objects: T[],
+	attributes: string[],
+): Promise<T[]> => {
+	if (_.isEmpty(objects)) {
+		return [] as T[];
+	}
+
+	const classNames: string[] = [];
+	// const objectsMapById = new Map<string, Parse.Object>();
+
+	_.forEach(objects, (object) => {
+		if (!object.id) {
+			throw new Error('Object has no id');
+		}
+		if (classNames.length > 1) {
+			throw new Error(
+				'Cannot fetch attributes on objects with different class names',
+			);
+		}
+		classNames.push(object.className);
+		// objectsMapById.set(object.id, object);
+	});
+
+	const objectsToFetchMap = new Map<
+		string,
+		{
+			object: Parse.Object;
+			attributes: string[];
+		}
+	>();
+
+	_.forEach(objects, (object) => {
+		const missingAttributes = attributes.filter(
+			(attribute) => !object.get(attribute),
+		);
+		if (!_.isEmpty(missingAttributes)) {
+			objectsToFetchMap.set(object.id, {
+				object,
+				attributes: missingAttributes,
+			});
+		}
+	});
+
+	const Collection = getDatabase().collection(classNames[0]);
+
+	const facetMatch: Record<string, PipelineStage[]> = {};
+
+	objectsToFetchMap.forEach((objectToFetch, objectId) => {
+		facetMatch[objectId] = [
+			{
+				$match: {
+					_id: objectId,
+				},
+			},
+			{
+				$project: {
+					_id: 1,
+					...objectToFetch.attributes,
+				},
+			},
+		];
+	});
+
+	const aggregateResult = Collection.aggregate([
+		{
+			$match: {
+				_id: {
+					$in: objects.map((object) => object.id),
+				},
+			},
+		},
+		{
+			$facet: facetMatch,
+		},
+	]);
+
+	const aggregateResultArray = await aggregateResult.toArray();
+
+	return _.map(objects, (object) => {
+		const clone = object.clone();
+		clone.id = object.id;
+
+		const newAttributes = _.get(aggregateResultArray[0], object.id);
+
+		if (!newAttributes) {
+			return clone;
+		}
+
+		const missingAttributes =
+			objectsToFetchMap.get(object.id)?.attributes || [];
+
+		clone.set(_.pick(newAttributes, missingAttributes));
+		return clone;
+	});
+};
--- apps/server/src/modules/common/auth/role/role.triggers.ts
+++ apps/server/src/modules/common/auth/role/role.triggers.ts
@@ -1,15 +1,17 @@
+import { HttpException } from '@/server/exceptions/HttpException';
+import { USE_MASTER_KEY } from '@/server/lib/constants';
 import { parseTriggerEnhanced } from '@/server/lib/parse/cloud/trigger';
+import StaffTenantService from '@/server/modules/staff/tenant/staff-tenant.service';
+import { roleEnum, roleSet, staffRoleSet } from '@/shared/lib/constants';
 import _ from 'lodash';
+import ParseUser from '../user/user.class';
 
 // --------------------------------------------------------------------------------------//
 //                                     BEFORE SAVE                                       //
 // --------------------------------------------------------------------------------------//
 
 const beforeSaveRole = parseTriggerEnhanced<Parse.Role>({
-	trigger: async ({ req, log }) => {
-		log.debug('🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯🎯', req.object.toJSON());
-		log.debug('😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂😂', req.original?.toJSON());
-
+	trigger: async ({ req, log, t }) => {
 		const json = req.object.toJSON();
 
 		// Check if we are assigning a role to a user
@@ -30,20 +32,117 @@ const beforeSaveRole = parseTriggerEnhanced<Parse.Role>({
 				'  rule 4: please add the new rules to this list if new requirements arise',
 			);
 			log.debug('============================================================');
+
+			const roleName = req.original?.get('name');
+
+			let userIds: string[] = [];
+
+			if (roleName) {
+				const _users = _.get(json, 'users.objects');
+				userIds = _.map(_users, (user) => user.objectId);
+			}
+
+			// if assigning TENANT_USER role to users
+			if (roleName && roleName === roleEnum.TENANT_USER.name) {
+				// check if any of the users is already a staff member
+				const staffTenantService = new StaffTenantService();
+
+				// find staff member roles at once
+				const staffMemberRoles = await new Parse.Query(Parse.Role)
+					.containedIn(
+						'name',
+						roleSet.STAFF_MEMBER.map((r) => r.name),
+					)
+					.select(['name'])
+					// it's ok to use master key here because we are in a trigger
+					.findAll(USE_MASTER_KEY);
+
+				const userIdsWithStaffMemberRole =
+					await staffTenantService.verifyIfAnyOfUsersHaveAnyOfRoles(
+						userIds,
+						staffMemberRoles,
+					);
+
+				if (!_.isEmpty(userIdsWithStaffMemberRole)) {
+					throw new HttpException(
+						400,
+						t('some-users-already-member-of-a-tenant'),
+						{
+							body: {
+								userIds: userIdsWithStaffMemberRole,
+							},
+						},
+					);
+				}
+			}
+
+			// if assigning STAFF_**** role to users
+			if (
+				roleName &&
+				_.values(staffRoleSet.STAFF_MEMBER).some(
+					(role) => role.name === roleName,
+				)
+			) {
+				// check if any of the users is already a tenant user
+				// step 1: check if users has not the role: TENANT_USER
+				// step 2: check if users have no relation with any tenant (look in the _CustomJoin:User:Tenant collection)
+
+				// step 1: check if users has not the role: TENANT_USER
+				const staffTenantService = new StaffTenantService();
+				const tenantUserRole = await new Parse.Query(Parse.Role)
+					.equalTo('name', roleEnum.TENANT_USER.name)
+					.select([])
+					// ok to use master key here because we ar in the scope of a trigger
+					.first(USE_MASTER_KEY);
+
+				if (!tenantUserRole) {
+					throw new HttpException(500, t('Internal server error'), {
+						meta: {
+							reason: 'Tenant user role not found',
+						},
+					});
+				}
+
+				const userIdsWithTenantUserRole =
+					await staffTenantService.verifyIfAnyOfUsersHaveAnyOfRoles(userIds, [
+						tenantUserRole,
+					]);
+
+				if (!_.isEmpty(userIdsWithTenantUserRole)) {
+					throw new HttpException(
+						400,
+						t('some-users-already-member-of-a-tenant'),
+						{
+							body: {
+								userIds: userIdsWithTenantUserRole,
+							},
+							meta: {
+								reason: `Some users has already the role: ${roleEnum.TENANT_USER.name}`,
+							},
+						},
+					);
+				}
+
+				// step 2: check if users have no relation with any tenant (look in the _CustomJoin:User:Tenant collection)
+				const userIdsWithTenant =
+					staffTenantService.verifyIfUsersHaveAnyTenant(userIds);
+
+				if (!_.isEmpty(userIdsWithTenant)) {
+					throw new HttpException(
+						400,
+						t('some-users-already-member-of-a-tenant'),
+						{
+							body: {
+								userIds: userIdsWithTenant,
+							},
+							meta: {
+								reason: 'Some users have already been assigned to a tenant',
+							},
+						},
+					);
+				}
+			}
 		}
-
-		// // Check if we are removing a role from a user
-		// const isRemovingRoleFromUser = req.original?.get('users') &&
-		// 	Array.isArray(req.original.get('users')) &&
-		// 	req.original.get('users').length > req.object.get('users')?.length;
-
-		// if (isRemovingRoleFromUser) {
-		// 	log.debug('🗑️ Removing role from user(s)', {
-		// 		roleName: req.object.get('name'),
-		// 		previousUsers: req.original?.get('users'),
-		// 		currentUsers: req.object.get('users'),
-		// 	});
-		// }
 	},
 });
 
