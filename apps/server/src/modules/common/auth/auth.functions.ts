3b34ff3215866389701c4de44a3189df12a0ecdd
Andrianarisoa Daniel <radandevist@gmail.com>
2025-06-22 06:38:02 +0300

    feat(exceptions): add meta property to HttpException and CloudFunctionHttpException
    - Introduce a new optional `meta` property in both `HttpException` and `CloudFunctionHttpException` classes for additional debugging information.
    - Update error handling in `checkEmailVerificationToken` to utilize the new `meta` property for improved context during error reporting.

--- apps/server/src/modules/common/auth/auth.functions.ts
+++ apps/server/src/modules/common/auth/auth.functions.ts
@@ -295,9 +295,10 @@ const checkEmailVerificationToken = fromPublicParseFunction({
 		if (isExpired) {
 			const error = new HttpException(400, t('Invalid token'), {
 				xcode: X_CODE.INVALID_EMAIL_VERIFICATION_TOKEN,
+				meta: {
+					reason: 'Session token expired',
+				},
 			});
-			// add additional information to the error
-			_.set(error, 'meta.cause', 'TOKEN_EXPIRED'); // only for debugging purposes, not to be exposed to the client
 			throw error;
 		}
 
