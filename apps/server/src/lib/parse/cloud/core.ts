87bdf83a1f82d6c48ca00dc24bc10d58e2ac1d30
Andrianarisoa Daniel <radandevist@gmail.com>
2025-06-22 17:10:10 +0300

    feat(migrations): implement staff member migration functions and update cloud function definitions
    - Add migration functions for `isStaffMember` and `roleData` to facilitate user role updates in the database.
    - Update `_migrations.ts` to run new migration functions for staff members.
    - Refactor `staff-member.functions.ts` to define and export staff member functions for better organization.
    - Modify `cloud.ts` to utilize the new staff member function definitions.
    - Clean up unused imports and commented-out code in `core-api.router.ts` for improved readability.

--- apps/server/src/lib/parse/cloud/core.ts
+++ apps/server/src/lib/parse/cloud/core.ts
@@ -287,12 +287,6 @@ export const cloudFunction: CloudFunction = <
 					message = error.message;
 				}
 
-				log.debug('🤔🤔🤔🤔🤔🤔🤔🤔🤔', {
-					isWhat: error instanceof HttpException,
-					meta: (error as HttpException).meta,
-					body: (error as HttpException).body,
-				});
-
 				log.error(hasMessage ? '' : message, error);
 
 				if (error instanceof HttpException) {
