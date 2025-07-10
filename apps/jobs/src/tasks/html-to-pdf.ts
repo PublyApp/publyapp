6aa9fbc5a90da35ad15c5238e42d76ac992bd6aa
Andrianarisoa Daniel <radandevist@gmail.com>
2025-07-10 09:29:05 +0300

    refactor(utils): rename sleep function to delay for clarity
    - Updated the `sleep` function to `delay` in `any.utils.ts` for better semantic understanding.
    - Adjusted references in `html-to-pdf.ts` to reflect the new function name, enhancing code readability.

--- apps/jobs/src/tasks/html-to-pdf.ts
+++ apps/jobs/src/tasks/html-to-pdf.ts
@@ -7,7 +7,7 @@ import { chromium } from 'playwright';
 import { z } from 'zod';
 
 import { className } from '@/shared/lib/constants';
-import { sleep } from '@/shared/utils/any.utils';
+import { delay } from '@/shared/utils/any.utils';
 
 import { getJobTypeFunction } from '../utils/utils';
 
@@ -201,7 +201,7 @@ const getControlledFunction = ({ handler }: { handler: AsyncFunction }) => {
 
 				do {
 					if (iterationIndex > 0) {
-						await sleep(intervalTime - elapsedTime);
+						await delay(intervalTime - elapsedTime);
 					}
 
 					const t1 = Date.now();
