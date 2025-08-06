e22b7812160c654d20d460056c97c67d54362146
Andrianarisoa Daniel <radandevist@gmail.com>
2025-08-06 11:35:53 +0300

    feat(address): implement address management components and billing integration
    - Added `AddressItem`, `AddressListDialog`, and `AddressNewForm` components for managing user addresses.
    - Integrated address management into the billing section with `AccountBillingAddress` and `AccountBillingPlan` components.
    - Implemented filtering and selection functionality for addresses and payment methods.
    - Created utility types for address and payment card management to enhance type safety and maintainability.

--- apps/front/app/routes/authed/staff/tenants/details/billing/tenant-details-billing-page.tsx
+++ apps/front/app/routes/authed/staff/tenants/details/billing/tenant-details-billing-page.tsx
@@ -1,5 +1,20 @@
+import {
+	_userAddressBook,
+	_userInvoices,
+	_userPayment,
+	_userPlans,
+} from '@/front/_mock';
+import { AccountBilling } from '@/front/components/billing/account-billing';
+
 const TenantDetailsBillingPage = () => {
-	return <div>TenantDetailsBillingPage</div>;
+	return (
+		<AccountBilling
+			plans={_userPlans}
+			cards={_userPayment}
+			invoices={_userInvoices}
+			addressBook={_userAddressBook}
+		/>
+	);
 };
 
 export default TenantDetailsBillingPage;
