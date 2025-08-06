25c756e7cdad73a226d5a95bbc79aa2f36019c99
Andrianarisoa Daniel <radandevist@gmail.com>
2025-08-06 11:35:53 +0300

    feat(address): implement address management components and billing integration
    - Added `AddressItem`, `AddressListDialog`, and `AddressNewForm` components for managing user addresses.
    - Integrated address management into the billing section with `AccountBillingAddress` and `AccountBillingPlan` components.
    - Implemented filtering and selection functionality for addresses and payment methods.
    - Created utility types for address and payment card management to enhance type safety and maintainability.

---
+++ apps/front/app/components/billing/account-billing-payment.tsx
@@ -0,0 +1,89 @@
+import type { IPaymentCard } from '@/front/types/common';
+import type { CardProps } from '@mui/material/Card';
+
+import { useBoolean } from 'minimal-shared/hooks';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+
+import { Iconify } from '../iconify/iconify';
+
+import { PaymentCardItem } from '../payment/payment-card-item';
+import { PaymentNewCardForm } from '../payment/payment-new-card-form';
+
+// ----------------------------------------------------------------------
+
+type Props = CardProps & {
+	cards: IPaymentCard[];
+};
+
+export function AccountBillingPayment({ cards, sx, ...other }: Props) {
+	const openForm = useBoolean();
+
+	return (
+		<>
+			<Card sx={[{ my: 3 }, ...(Array.isArray(sx) ? sx : [sx])]} {...other}>
+				<CardHeader
+					title="Payment method"
+					action={
+						<Button
+							size="small"
+							color="primary"
+							startIcon={<Iconify icon="mingcute:add-line" />}
+							onClick={openForm.onTrue}
+						>
+							New card
+						</Button>
+					}
+				/>
+
+				<Box
+					sx={{
+						p: 3,
+						rowGap: 2.5,
+						columnGap: 2,
+						display: 'grid',
+						gridTemplateColumns: { xs: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' },
+					}}
+				>
+					{cards.map((card) => (
+						<PaymentCardItem key={card.id} card={card} />
+					))}
+				</Box>
+			</Card>
+
+			<Dialog
+				fullWidth
+				maxWidth="xs"
+				open={openForm.value}
+				onClose={openForm.onFalse}
+			>
+				<DialogTitle> Add new card </DialogTitle>
+
+				<DialogContent sx={{ overflow: 'unset' }}>
+					<PaymentNewCardForm />
+				</DialogContent>
+
+				<DialogActions>
+					<Button color="inherit" variant="outlined" onClick={openForm.onFalse}>
+						Cancel
+					</Button>
+
+					<Button
+						color="inherit"
+						variant="contained"
+						onClick={openForm.onFalse}
+					>
+						Add
+					</Button>
+				</DialogActions>
+			</Dialog>
+		</>
+	);
+}
