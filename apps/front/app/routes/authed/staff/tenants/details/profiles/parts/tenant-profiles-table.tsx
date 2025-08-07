9c6fdff55fc63289c1829f1f77b3fe95770b9df5
Andrianarisoa Daniel <radandevist@gmail.com>
2025-08-07 19:48:06 +0300

    refactor(tenant-profiles-table): streamline column definitions and enhance placeholder logic
    - Consolidated column definitions by extracting common columns into a reusable array for better maintainability.
    - Improved the placeholder column logic to utilize the common columns, ensuring consistent styling and structure.
    - Removed redundant code related to column definitions, enhancing clarity and performance of the component.

--- apps/front/app/routes/authed/staff/tenants/details/profiles/parts/tenant-profiles-table.tsx
+++ apps/front/app/routes/authed/staff/tenants/details/profiles/parts/tenant-profiles-table.tsx
@@ -1,7 +1,3 @@
-import type { TenantProfile } from '@/front/_mock/_tenant-profiles';
-import { useMRTTable } from '@/front/hooks/use-mrt-table';
-import { useFindTenantProfiles } from '@/front/lib/react-query/features/tenant/tenant.hooks';
-import { TENANT_PROFILES_PERMISSIONS_ENUM } from '@/shared/lib/constants';
 import Card from '@mui/material/Card';
 import Checkbox from '@mui/material/Checkbox';
 import Skeleton from '@mui/material/Skeleton';
@@ -13,10 +9,40 @@ import {
 } from 'material-react-table';
 import { useMemo } from 'react';
 import { useParams } from 'react-router';
+import type { TenantProfile } from '@/front/_mock/_tenant-profiles';
+import { useMRTTable } from '@/front/hooks/use-mrt-table';
+import { useFindTenantProfiles } from '@/front/lib/react-query/features/tenant/tenant.hooks';
+import { TENANT_PROFILES_PERMISSIONS_ENUM } from '@/shared/lib/constants';
 
 const columnHelper = createMRTColumnHelper<
 	Record<string, unknown> & { permission: string }
 >();
+
+const commonColumns = [
+	columnHelper.accessor('permission', {
+		header: 'Permission',
+		Cell: ({ cell }) => {
+			return <>{cell.getValue()}</>;
+		},
+		size: 250, // ! no choice but to set a fixed size
+	}),
+];
+
+const placeholderColumns = [
+	...commonColumns,
+	...Array.from({ length: 10 }, (_, index) => {
+		return columnHelper.accessor(`placeholder-${index}`, {
+			id: `placeholder-${index}`,
+			header: `Placeholder ${index}`,
+			Header: () => {
+				return <Skeleton variant="text" width="100%" height="100%" />;
+			},
+			Cell: () => {
+				return <Skeleton variant="text" width="100%" height="100%" />;
+			},
+		});
+	}),
+];
 
 const ALL_PERMISSIONS = _.values(TENANT_PROFILES_PERMISSIONS_ENUM);
 
@@ -48,15 +74,7 @@ const TenantProfilesTable = () => {
 	const theme = useTheme();
 
 	const columns = useMemo(() => {
-		const columnsDefinition = [
-			columnHelper.accessor('permission', {
-				header: 'Permission',
-				Cell: ({ cell }) => {
-					return <>{cell.getValue()}</>;
-				},
-				size: 250, // ! no choice but to set a fixed size
-			}),
-		];
+		const columnsDefinition = commonColumns;
 
 		profilesMap.forEach((profile) => {
 			columnsDefinition.push(
@@ -76,24 +94,6 @@ const TenantProfilesTable = () => {
 		});
 		return columnsDefinition;
 	}, [profilesMap]);
-
-	const placeholderColumns = useMemo(() => {
-		return [
-			...columns,
-			...Array.from({ length: 10 }, (_, index) => {
-				return columnHelper.accessor(`placeholder-${index}`, {
-					id: `placeholder-${index}`,
-					header: `Placeholder ${index}`,
-					Header: () => {
-						return <Skeleton variant="text" width="100%" height="100%" />;
-					},
-					Cell: () => {
-						return <Skeleton variant="text" width="100%" height="100%" />;
-					},
-				});
-			}),
-		];
-	}, [columns]);
 
 	const table = useMRTTable('default', {
 		columns: isPending ? placeholderColumns : columns,
@@ -124,12 +124,9 @@ const TenantProfilesTable = () => {
 					boxShadow: 'unset',
 					borderRight: `1px dashed ${theme.vars.palette.divider}`,
 					backgroundColor: 'var(--permission-column-bg)',
-					// backgroundColor: 'red',
-					// backgroundColor: 'white',
 				},
 				'& th[data-pinned="true"], & td[data-pinned="true"]': {
 					opacity: 1,
-					// backgroundColor: 'blue',
 				},
 			},
 		},
