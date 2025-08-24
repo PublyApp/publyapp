53bd5309577e42308a9340e0f51165fb0510f2d9
Andrianarisoa Daniel <radandevist@gmail.com>
2025-08-24 08:36:19 +0300

    refactor(MainApi): implement custom exception handling middleware
    - Introduced CustomExceptionHandler class to handle exceptions globally and return structured JSON responses.
    - Removed the obsolete ExceptionHandlerExtensions class to streamline the codebase.
    - Updated Program.cs to utilize the new custom exception handling middleware, enhancing error management.

--- apps/MainApi/Program.cs
+++ apps/MainApi/Program.cs
@@ -5,37 +5,33 @@ using MainApi.Src.Features.Common.Auth;
 using MainApi.Src.Features.Tenant.Product;
 using MainApi.Src.Features.Common.Auth.Middlewares;
 
+AppEnvironment.LoadDotEnv();
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Configure Serilog
 builder.ConfigureSerilog();
-
 builder.AddServices();
 
 var app = builder.Build();
 
 app.UseCustomExceptionHandler();
 
-// dev only middlewares
 if (app.Environment.IsDevelopment())
 {
 	app.MapOpenApi();
 	app.MapScalarApiReference();
 }
 
-// all-time middlewares
 app.UseCheckSessionHeader();
 app.UseCheckTenantHeader();
 app.UseSessionAuthentication();
 // TODO: UseTenantAuthentication();
 
-// mount endpoints
 app.MapAuthEndpoints();
 
 var staffGroup = app.MapGroup("/staff");
 var tenantGroup = app.MapGroup("/tenant");
 
-// Map organized endpoints
 tenantGroup.MapProductEndpoints();
 
 app.MapFallback(() => Results.NotFound(new
