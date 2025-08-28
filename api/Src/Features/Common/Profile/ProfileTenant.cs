b0f089df8b0c67a6bd210a83eda1608b08f80a9e
Andrianarisoa Daniel <radandevist@gmail.com>
2025-08-28 11:38:01 +0300

    feat(ProfileManagement): enhance ProfileStaff and ProfileTenant models with additional properties
    - Added Name, Description, and Permissions properties to ProfileStaff and ProfileTenant classes for improved profile management.
    - Updated MongoDB serialization attributes for new properties to ensure proper data handling.
    - Introduced StaffPermissionFilter enhancements to support flexible permission checks based on user profiles, improving access control capabilities.
    - Added comprehensive documentation for StaffPermissionFilter, detailing usage, custom logic, and best practices for permission management.

--- api/Src/Features/Common/Profile/ProfileTenant.cs
+++ api/Src/Features/Common/Profile/ProfileTenant.cs
@@ -1,8 +1,21 @@
 namespace MainApi.Src.Features.Common.Profile;
 
 using MainApi.Src.Data;
+using MongoDB.Bson.Serialization.Attributes;
 
 public class ProfileTenant : BaseAttributes, ITenantEntity
 {
+	[BsonElement("tenantId")]
 	public string TenantId { get; set; } = string.Empty;
+
+	[BsonElement("name")]
+	public string? Name { get; set; }
+
+	[BsonElement("description")]
+	public string? Description { get; set; }
+
+	[BsonElement("permissions")]
+	public List<string>? Permissions { get; set; }
+
+	public static readonly string CollectionName = "_ProfileTenant";
 }
