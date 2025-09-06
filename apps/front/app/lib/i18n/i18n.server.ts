cd67ab3bf1c809ff2646d1e7cf66fd28ad0dfaf4
Andrianarisoa Daniel <radandevist@gmail.com>
2025-09-06 09:08:13 +0300

    refactor(i18n): improve logging in i18n file copying and update load paths
    - Introduced a logging mechanism to toggle console output during file copying.
    - Updated load paths for translation files to reflect new directory structure in development and production environments.

--- apps/front/app/lib/i18n/i18n.server.ts
+++ apps/front/app/lib/i18n/i18n.server.ts
@@ -1,5 +1,5 @@
-import { resolve } from 'node:path';
 import Backend from 'i18next-fs-backend';
+import { resolve } from 'node:path';
 import { RemixI18Next } from 'remix-i18next/server';
 import { config } from './i18n.config';
 
@@ -13,9 +13,12 @@ export const remixI18NextServer = new RemixI18Next({
 	i18next: {
 		...config,
 		backend: {
+			// loadPath: import.meta.env.DEV
+			// 	? resolve(process.cwd(), '../server/dist/resources/{{lng}}.{{ns}}.json')
+			// 	: resolve(process.cwd(), './dist/resources/{{lng}}.{{ns}}.json'),
 			loadPath: import.meta.env.DEV
-				? resolve(process.cwd(), '../server/dist/resources/{{lng}}.{{ns}}.json')
-				: resolve(process.cwd(), './dist/resources/{{lng}}.{{ns}}.json'),
+				? resolve(process.cwd(), './public/tx/{{lng}}.{{ns}}.json')
+				: resolve(process.cwd(), './build/client/tx/{{lng}}.{{ns}}.json'),
 		},
 	},
 	// The i18next plugins you want RemixI18next to use for `i18n.getFixedT` inside loaders and actions.
