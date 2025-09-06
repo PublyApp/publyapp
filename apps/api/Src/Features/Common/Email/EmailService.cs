869607dfe2a2cff0dad74ad25dc72de2ceaa8809
Andrianarisoa Daniel <radandevist@gmail.com>
2025-09-06 14:51:57 +0300

    feat(api): enhance email verification process and introduce encoding utilities
    - Added EMAIL_VERIFY_TOKEN_VALIDITY_DURATION setting to appsettings.json for configurable token expiration.
    - Implemented asynchronous email sending in VerifyEmailRequest to improve performance.
    - Introduced AppEncoder utility for secure string encoding and decoding, enhancing email verification link security.
    - Updated EmailService to include a method for sending verification emails with a generated link.
    - Refactored UserService to support user updates for email verification tokens.

--- apps/api/Src/Features/Common/Email/EmailService.cs
+++ apps/api/Src/Features/Common/Email/EmailService.cs
@@ -1,8 +1,12 @@
+using MainApi.Src.Lib;
+using MainApi.Src.Lib.Utils;
+
 namespace MainApi.Src.Features.Common.Email;
 
 public interface IEmailService
 {
 	Task SendEmail(string email, string subject, string body);
+	Task SendVerificationMail(string email, string token);
 }
 
 public class EmailService : IEmailService
@@ -19,5 +23,19 @@ public class EmailService : IEmailService
 		// TODO: Implement email sending
 		await Task.Delay(1000);
 		_logger.LogWarning("Simulate sending email {@EmailData}", new { email, subject, body });
+	}
+
+	public static string CreateVerificationLink(string token, string email)
+	{
+		return $"{AppEnvironment.FRONT_URL}/verify-email?token={token}&id={AppEncoder.EncodeString(email)}";
+	}
+
+	public async Task SendVerificationMail(string email, string token)
+	{
+		await SendEmail(
+				email,
+				"Email Verification",
+				"Please verify your email by clicking the link below:\n\t👉" + CreateVerificationLink(token, email)
+			);
 	}
 }
