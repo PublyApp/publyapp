e4d569a1473a3265396d34a849645aadc93280cd
Andrianarisoa Daniel <radandevist@gmail.com>
2026-01-13 07:53:17 +0300

    chore: move the default react router ap directory from 'app' to 'src'

--- apps/front/react-router.config.ts
+++ apps/front/react-router.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from '@react-router/dev/config';
 
 export default {
-	// Config options...
+	// override the default app directory
+	appDirectory: 'src',
+
 	// Server-side render by default, to enable SPA mode set this to `false`
 	ssr: true,
 
