5fd82c5355a3c4b2dc033ec13185969b5db81520
Dan <radandevist@gmail.com>
2026-04-13 19:44:55 +0300

    feat(staff): profile users tab, assign drawer, and bulk seed staff users
    - Wire staff profile users table to the API and add assign-users drawer UX
    - Add staff profile users hooks; extend EN/FR common strings
    - Extend bulk seed data (incl. staff users), tweak BulkSeeder, update justfile note
    - Add bulk-seeding utilities doc; expand staff profiles smoke-test checklist

--- apps/front/src/routes/authed/staff/profiles/details/users/staff-profile-details-users-tab-page.tsx
+++ apps/front/src/routes/authed/staff/profiles/details/users/staff-profile-details-users-tab-page.tsx
@@ -2,11 +2,19 @@ import Box from '@mui/material/Box';
 import capitalize from 'lodash/capitalize';
 import toStr from 'lodash/toString';
 import { useOutletContext, useParams } from 'react-router';
+import { useQueryClient } from '@tanstack/react-query';
 
 import { FRONT_PATH_NAMES } from '@org/shared-ts/lib/constants';
 
 import { CustomBreadcrumbs } from '#app/components/custom-breadcrumbs/custom-breadcrumbs.tsx';
 import { useTranslate } from '#app/hooks/use-translate.ts';
+import { getUntypedNumber } from '#app/lib/js-client/kiota-utils.ts';
+import { useFindStaffProfileUsers } from '#app/lib/react-query/features/staff/staff-profile.hooks.ts';
+import {
+	useFindStaffUser,
+	useGetStaffUserProfiles,
+	useUpdateStaffUserProfiles,
+} from '#app/lib/react-query/features/staff/staff-user.hooks.ts';
 import type { StaffProfileDetailsOutletContext } from '../_layout/staff-profile-details-layout';
 
 import type { StaffProfileDetailsOutletContext } from '../_layout/staff-profile-details-layout';
@@ -15,6 +23,7 @@ import { UserAssignDrawer } from './parts/user-assign-drawer.tsx';
 
 const StaffProfileDetailsUsersTabPage = () => {
 	const { t } = useTranslate();
+	const queryClient = useQueryClient();
 	const { profileId } = useParams();
 	const { profileName } = useOutletContext<StaffProfileDetailsOutletContext>();
 	const resolvedProfileId = toStr(profileId);
