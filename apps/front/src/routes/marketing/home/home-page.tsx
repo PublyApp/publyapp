caf0f903278b6e34c141a5bd061c87ae585e4340
Dan <radandevist@gmail.com>
2026-05-02 20:14:29 +0300

    refactor(front): lift ScrollProgress + BackToTop to MarketingLayout

--- apps/front/src/routes/marketing/home/home-page.tsx
+++ apps/front/src/routes/marketing/home/home-page.tsx
@@ -1,7 +1,3 @@
-import { BackToTopButton } from '#app/components/animate/back-to-top-button.tsx';
-import { ScrollProgress } from '#app/components/animate/scroll-progress/scroll-progress.tsx';
-import { useScrollProgress } from '#app/components/animate/scroll-progress/use-scroll-progress.ts';
-
 import type { Route } from './+types/home-page';
 import { HomeCta } from './parts/home-cta';
 import { HomeFaq } from './parts/home-faq';
@@ -12,22 +8,8 @@ import { HomeOnboarding } from './parts/home-onboarding';
 import { HomePricing } from './parts/home-pricing';
 
 const HomePage = ({ loaderData: _ }: Route.ComponentProps) => {
-	const pageProgress = useScrollProgress();
-
 	return (
 		<>
-			<ScrollProgress
-				variant="linear"
-				progress={pageProgress.scrollYProgress}
-				sx={[
-					(theme) => {
-						return { position: 'fixed', zIndex: theme.zIndex.appBar + 1 };
-					},
-				]}
-			/>
-
-			<BackToTopButton />
-
 			<HomeHero />
 			<HomeLogos />
 			<HomeFeatures />
