ef5eacffe4f93657c187f12d1366f75ef2ac941d
Dan <radandevist@gmail.com>
2026-05-05 08:34:59 +0300

    chore: remove resurrected rebase artifacts

--- apps/server/src/@types/global.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-var */
-// eslint-disable-next-line prettier/prettier
-export {};
-
-declare global {
-	var LOCAL: boolean;
-	var PRODUCTION: boolean;
-	var FORCE_PROD: boolean;
-}
