b4adbac80e797a8f4df99ba55133ddf9cc7425b3
Andrianarisoa Daniel <radandevist@gmail.com>
2026-07-03 13:00:18 +0000

    fix(front-2): guard /staff redirect to exact path (Refs #722)

--- apps/front-2/src/routes/authed/staff.tsx
+++ apps/front-2/src/routes/authed/staff.tsx
@@ -1,10 +1,13 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/_authed-layout/staff')({
-	beforeLoad: () => {
-		throw redirect({
-			to: '/staff/staff-users',
-		});
+	beforeLoad: ({ location }) => {
+		const pathname = location.pathname ?? '';
+		if (pathname === '/staff') {
+			throw redirect({
+				to: '/staff/staff-users',
+			});
+		}
 	},
 	component: StaffShellPlaceholder,
 });
