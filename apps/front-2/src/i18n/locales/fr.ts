6cce3241ebb41f2fbf7f8e009bc5041fdae11259
Andrianarisoa Daniel <radandevist@gmail.com>
2026-07-23 12:40:45 +0300

    style(front-2): format locale imports

--- apps/front-2/src/i18n/locales/fr.ts
+++ apps/front-2/src/i18n/locales/fr.ts
@@ -3,9 +3,9 @@ import sharedFr from '@org/shared-ts/lib/i18n/locales/fr';
 import type { LooseResource } from './en';
 import auth from './fr/auth.json';
 import common from './fr/common.json';
+import staffInvitations from './fr/staff-invitations.json';
 import staffTenantProfiles from './fr/staff-tenant-profiles.json';
 import staffUsers from './fr/staff-users.json';
-import staffInvitations from './fr/staff-invitations.json';
 
 const resourceFR = {
 	common,
