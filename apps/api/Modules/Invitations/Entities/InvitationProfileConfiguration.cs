350d9c91e34e45d961c633878a0922f234491043
Andrianarisoa Daniel <radandevist@gmail.com>
2026-08-01 09:20:52 +0300

    refactor(api): extract invitation configs

---
+++ apps/api/Modules/Invitations/Entities/InvitationProfileConfiguration.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using PublyApp.Api.Data.DbContext;
+
+namespace PublyApp.Api.Modules.Invitations.Entities;
+
+public sealed class InvitationProfileConfiguration : IEntityTypeConfiguration<InvitationProfile> {
+	public void Configure(EntityTypeBuilder<InvitationProfile> builder) {
+		EntityConfigurationMarker.Mark(builder);
+
+		// Configure InvitationProfile junction table
+		builder.HasKey(entity => new { entity.InvitationId, entity.ProfileId });
+
+		builder
+			.HasOne(entity => entity.Invitation)
+			.WithMany(invitation => invitation.InvitationProfiles)
+			.HasForeignKey(entity => entity.InvitationId)
+			.OnDelete(DeleteBehavior.Cascade);
+
+		builder
+			.HasOne(entity => entity.Profile)
+			.WithMany()
+			.HasForeignKey(entity => entity.ProfileId)
+			.OnDelete(DeleteBehavior.Restrict);
+	}
+}
